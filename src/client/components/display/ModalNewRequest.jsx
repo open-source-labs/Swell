79dbc62f5adf905aa00e79b01f4c6ce0f737b0d9
brandon6190 <brandon6190@outlook.com>
2018-11-14 21:15:00 -0500

    eslint fixes

--- src/client/components/display/ModalNewRequest.jsx
+++ src/client/components/display/ModalNewRequest.jsx
@@ -3,12 +3,8 @@ import { connect } from "react-redux";
 import Request from "./Request.jsx";
 import Response from "./Response.jsx";
 
-<<<<<<< HEAD
-import * as actions from "../../actions/actions";
-=======
 import * as actions from '../../actions/actions';
 import HeaderEntryForm from './HeaderEntryForm.jsx';
->>>>>>> dev
 
 const mapStateToProps = store => ({});
 
@@ -22,19 +18,11 @@ class ModalNewRequest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
-      method: "",
-      headers: "",
-      body: "",
-      url: ""
-    };
-=======
       method : 'GET',
       headers : [],
       body : '',
       url : '',
     }
->>>>>>> dev
     this.methodOnChange = this.methodOnChange.bind(this);
     this.updateHeaders = this.updateHeaders.bind(this);
     this.bodyOnChange = this.bodyOnChange.bind(this);
@@ -52,46 +40,19 @@ class ModalNewRequest extends Component {
       url: e.target.value
     });
   }
-<<<<<<< HEAD
-  headersOnChange(e) {
-    this.setState({
-      headers: e.target.value
-=======
-  updateHeaders (headers) {
-    console.log('UPDATE HEADERS CALLED', headers);
-    this.setState({
-      headers: headers.filter(header => {
-        return header.active;
       }),
     },() => {
       console.log(this.state.headers)
->>>>>>> dev
     });
   }
   bodyOnChange(e) {
     this.setState({
       body: e.target.value
-    });
   }
 
   addNewRequest() {
     console.log(this.state.headers)
     let reqRes = {
-<<<<<<< HEAD
-      id: Math.random() * 100000,
-      url: this.state.url,
-      timeSent: null,
-      timeReceived: null,
-      request: {
-        method: this.state.method,
-        headers: JSON.parse(this.state.headers),
-        body: JSON.parse(this.state.body)
-      },
-      response: {
-        headers: null,
-        data: null,
-        type: null
-=======
       id : Math.floor(Math.random() * 100000),
       url : this.state.url,
       timeSent : null,
@@ -106,7 +67,6 @@ class ModalNewRequest extends Component {
       response : {
         headers : null,
         events : null,
->>>>>>> dev
       }
     };
 
@@ -114,46 +74,6 @@ class ModalNewRequest extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-    console.log(this.state);
-    return (
-      <div
-        style={{
-          border: "1px solid black",
-          display: "flex",
-          flexDirection: "column"
-        }}
-      >
-        ModalNewRequest
-        <input
-          type="text"
-          placeholder="Method"
-          onChange={e => {
-            this.methodOnChange(e);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="URL"
-          onChange={e => {
-            this.urlOnChange(e);
-          }}
-        />
-        <textarea
-          type="text"
-          placeholder="Headers"
-          onChange={e => {
-            this.headersOnChange(e);
-          }}
-        />
-        <textarea
-          type="text"
-          placeholder="Body"
-          onChange={e => {
-            this.bodyOnChange(e);
-          }}
-        />
-=======
     return(
       <div style={{'border' : '1px solid black', 'display' : 'flex', 'flexDirection' : 'column'}}>
         ModalNewRequest
@@ -177,7 +97,6 @@ class ModalNewRequest extends Component {
           this.bodyOnChange(e)
         }}></textarea>
 
->>>>>>> dev
         <button onClick={this.addNewRequest}>Add New Request</button>
       </div>
     );
