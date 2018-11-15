8e363c553f0661149f6bbab204ac2232c0e6cb14
Jason <jason.ou@nyu.edu>
2018-11-15 11:10:21 -0500

    updates to reducer and display for req/res

--- src/client/components/display/Response.jsx
+++ src/client/components/display/Response.jsx
@@ -18,11 +18,23 @@ class Response extends Component {
 
 
   render() {
+    // let headersArr = this.props.content.headers.map((header, index) => {
+    //   console.log(header);
+    //   return (<div key={index}>{header}</div>)
+    // });
+
+    let eventsArr = this.props.content.events.map((event, index) => {
+      return (<div style={{'display' : 'flex'}} key={index}>
+          <div style={{'width' : '50%'}}>{event.data}</div>
+          <div style={{'width' : '50%'}}>{event.timeReceived}</div>
+        </div>)
+    });
+  
     return(
       <div style={{'border' : '1px solid black', 'margin' : '3px', 'display' : 'flex', 'flexDirection' : 'column'}}>
         Response
-        {this.props.content.headers}
-        {this.props.content.data}
+        {/* {headersArr} */}
+        {eventsArr}
       </div>
     )
   }
