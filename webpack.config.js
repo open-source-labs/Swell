a80e1e91b48c36a6b4709a037804f3873cfc08d8
Kyle Combs <combskyle@gmail.com>
2018-11-19 19:04:04 -0500

    restructure node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -34,10 +34,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.styl$/, 
-        use: [ 'style-loader', 'css-loader', 'stylus-loader' ]
       }
     ],
   },
