ac21caeb935278aab9c775d884cbd6511ef52f9a
Kyle Combs <combskyle@gmail.com>
2018-11-21 11:21:04 -0500

    index cleanup

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import store from './client/store';
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
 
-const Hello = () => <p>Hello World</p>
-
 root.id = 'root'
 document.body.appendChild(root)
 
