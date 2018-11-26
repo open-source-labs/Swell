200a0b770b774932f5f49baad2ca94fbe2a5384f
brandon6190 <brandon6190@outlook.com>
2018-11-26 16:09:36 -0500

    created .eslintrc.js file for eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,17 @@ module.exports = {
 		"react",
 		"jsx-a11y",
 		"import"
-	]
+	],
+	"rules": {
+		"react/jsx-filename-extension": 0,
+		"no-use-before-define": 0,
+		"no-console": 0,
+		"func-names": 0,
+		"no-undef": 0,
+		"no-unused-vars": 0,
+		"strict": 0,
+		"dot-notation": 0,
+		"max-len": 0,
+		"object-curly-newline": 0
+	}
 };
