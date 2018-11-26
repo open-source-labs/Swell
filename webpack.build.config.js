0f572e90b992d6e8a2187a4a6b273d0d58f11552
brandon6190 <brandon6190@outlook.com>
2018-11-26 17:05:07 -0500

    fixed eslin problems in both webpack files

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,11 +1,12 @@
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BabiliPlugin = require('babili-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+import webpack from 'webpack';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BabiliPlugin = require('babili-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
-const defaultInclude = path.resolve(__dirname, 'src')
+const defaultInclude = path.resolve(__dirname, 'src');
 
 module.exports = {
   module: {
@@ -16,33 +17,33 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           'css-loader',
         ],
-        include: defaultInclude
+        include: defaultInclude,
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader', 
-          'css-loader', 
-          'sass-loader'
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-        include: defaultInclude
+        include: defaultInclude,
       },
       {
         test: /\.jsx?$/,
         use: [{ loader: 'babel-loader' }],
-        include: defaultInclude
+        include: defaultInclude,
       },
       {
         test: /\.(jpe?g|png|gif)$/,
         use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
+        include: defaultInclude,
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude
-      }
-    ]
+        include: defaultInclude,
+      },
+    ],
   },
   target: 'electron-renderer',
   plugins: [
@@ -51,17 +52,17 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: 'bundle.css',
-      chunkFilename: '[id].css'
+      chunkFilename: '[id].css',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    new BabiliPlugin()
+    new BabiliPlugin(),
   ],
   stats: {
     colors: true,
     children: false,
     chunks: false,
-    modules: false
-  }
-}
+    modules: false,
+  },
+};
