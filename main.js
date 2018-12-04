0fbd50d8500f38faf08f6bb67828af4004543a1c
Kyle Combs <combskyle@gmail.com>
2018-12-04 11:02:54 -0500

    final build for launch

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ const url = require('url')
 const { autoUpdater } = require('electron-updater')
 const log = require('electron-log');
 
-const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
-
 const { TouchBarButton, TouchBarSpacer } = TouchBar;
 
 // // configure logging
@@ -108,14 +106,15 @@ function createWindow() {
   })
 
   if (dev) {
-  // Adding React & Redux DevTools to Electon App
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err));
+    const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+  Adding React & Redux DevTools to Electon App
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
 
-  installExtension(REDUX_DEVTOOLS)
-    .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err));
+    installExtension(REDUX_DEVTOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
   }
 
   // and load the index.html of the app.
