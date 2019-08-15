3df5db019823e841855460aefc22ff38b30422d4
Kajol Thapa <kajoolthapa@gmail.com>
2019-08-15 10:57:19 -0400

    cleaned up graphQLController - removed unused library

--- src/client/controllers/graphQLController.js
+++ src/client/controllers/graphQLController.js
@@ -2,16 +2,14 @@ import ApolloClient from 'apollo-client';
 import gql from 'graphql-tag';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+
 import * as store from '../store';
 import * as actions from '../actions/actions';
-import { ApolloLink } from 'apollo-link';
 
 
 const graphQLController = {
 
   openGraphQLConnection(reqResObj) {
-    console.warn(reqResObj)
     // initialize response data
     reqResObj.response.headers = {};
     reqResObj.response.events = [];
@@ -25,32 +23,20 @@ const graphQLController = {
       headers[item.key] = item.value;
     });
 
-    const httpLink = createHttpLink({ uri: reqResObj.url });
-    const middlewareLink = new ApolloLink((operation, forward) => {
-      operation.setContext({
-        headers: {
-          authorization: localStorage.getItem('token') || null
-        }
-      });
-      return forward(operation)
-    }) 
-    
     const client = new ApolloClient({
-      link: middlewareLink.concat(httpLink),
+      link: createHttpLink({ uri: reqResObj.url }),
+      headers,
       credentials: 'same-origin',
       cache: new InMemoryCache(),
     });
-    console.log(client,'**********8888888888888888888')
+
     const body = gql`${reqResObj.request.body}`;
     const variables = reqResObj.request.bodyVariables ? JSON.parse(reqResObj.request.bodyVariables) : {};
 
     if (reqResObj.request.method === 'QUERY') {
       client.query({ query: body, variables })
         // Update the store with the response
-        .then(data => {
-          console.log({body, variables})
-          return this.handleResponse(data, reqResObj)
-        })
+        .then(data => this.handleResponse(data, reqResObj))
         .catch((err) => {
           console.error(err);
           reqResObj.connection = 'error';
@@ -75,7 +61,6 @@ const graphQLController = {
   },
 
   handleResponse(response, reqResObj) {
-    console.log(response, reqResObj)
     const reqResCopy = JSON.parse(JSON.stringify(reqResObj));
     // TODO: Add response headers, cookies
     reqResCopy.connection = 'closed';
