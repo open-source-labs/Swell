0abacf535bb84519edd8a52d833019b036cfb111
Nancy Dao <nancyddao@gmail.com>
2020-01-23 12:37:16 -0800

    fixed bug causing http button to toggle on when selecting grpc

--- src/client/components/composer/NewRequest/GRPCAutoInputForm.jsx
+++ src/client/components/composer/NewRequest/GRPCAutoInputForm.jsx
@@ -8,37 +8,37 @@ class GRPCAutoInputForm extends Component {
     this.state = {
       show: true,
       // ***MOCK DATA***
-      services: [
-        {
-          name: 'BookService',
-          rpcs: [
-            {
-              name: 'GetBook',
-              type: 'unary',
-              definition: 'rpc GetBook (GetBookRequest) returns (Book) {}',
-              messages: [
-                {
-                  name: Book,
-                  definition: [
-                    {
-                      number: 1,
-                      definition: 'int64 isbn = 1'
-                    },
-                    {
-                      number: 2,
-                      definition: 'string title = 2'
-                    },
-                    {
-                      number: 3,
-                      definition: 'string author = 3'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      ]
+      // services: [
+      //   {
+      //     name: 'BookService',
+      //     rpcs: [
+      //       {
+      //         name: 'GetBook',
+      //         type: 'unary',
+      //         definition: 'rpc GetBook (GetBookRequest) returns (Book) {}',
+      //         messages: [
+      //           {
+      //             name: 'Book',
+      //             definition: [
+      //               {
+      //                 number: 1,
+      //                 definition: 'int64 isbn = 1'
+      //               },
+      //               {
+      //                 number: 2,
+      //                 definition: 'string title = 2'
+      //               },
+      //               {
+      //                 number: 3,
+      //                 definition: 'string author = 3'
+      //               }
+      //             ]
+      //           }
+      //         ]
+      //       }
+      //     ]
+      //   }
+      // ]
     };
  
     this.toggleShow = this.toggleShow.bind(this);
@@ -56,19 +56,16 @@ class GRPCAutoInputForm extends Component {
   //     services: mockData.services,
   //   });
   // }
-
-  loadServices() {
-    let services = this.state.services;
-    let optionsList = [];
-    for (let i = 0; i < services.length; i++) {
-      optionsList.push(<option value={i}>services[i].name</option>)
-    }
-    return optionsList;
-  }
+  
 
   render() {
     const arrowClass = this.state.show ? 'composer_subtitle_arrow-open' : 'composer_subtitle_arrow-closed';
     const bodyContainerClass = this.state.show ? 'composer_bodyform_container-open' : 'composer_bodyform_container-closed';
+
+    // let services = this.state.services;
+    // for (let i = 0; i < services.length; i++) {
+    //   <option value={i}>services[i].name</option>
+    // }
 
     return (
       <div >
@@ -79,8 +76,7 @@ class GRPCAutoInputForm extends Component {
 
        <select id="dropdownService" name="dropdownService" className={'dropdownService ' + bodyContainerClass}>
           <option value="test1" selected="">Service1</option>
-          
-          <option value="test2">Service2</option>
+          {/* {optionsList} */}
         </select>
 
         <select id="dropdownRequest" name="dropdownRequest" className={'dropdownRequest ' + bodyContainerClass}>
