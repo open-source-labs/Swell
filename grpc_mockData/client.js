5b8b652d6e2d1789dd30424cf9ffb329f0431abe
Yoon  Choi <yoonsnewmac@Yoons-MacBook-Pro.local>
2020-02-01 11:47:50 -0800

    grpc-testing update

--- grpc_mockData/client.js
+++ grpc_mockData/client.js
@@ -1,4 +1,4 @@
-var path = require("path");
+const path = require("path");
 const protoLoader = require("@grpc/proto-loader");
 const grpc = require("grpc");
 
@@ -9,22 +9,25 @@ const loaded = grpc.loadPackageDefinition(pd);
 const hello_proto = loaded.helloworld;
 
 function main() {
-  var client = new hello_proto.Greeter(
+  const client = new hello_proto.Greeter(
     "localhost:50051",
     grpc.credentials.createInsecure()
   );
-  var user;
+  // set request value
+  let user;
+  // returns an array containing the command line arguments passed when the Node.js process was launched
+  // starts on 2 because process.argv contains the whole command-line invocation
+  // argv[0] and argv[1] will be node example.js
   if (process.argv.length >= 3) {
     user = process.argv[2];
   } else {
     user = "world";
   }
   
+  // create metadata
   const meta = new grpc.Metadata();
   meta.add("testing", "metadata is working");
-  // client.sayHello.send({name: user}, meta, function(err, response) {
-  //   console.log("metadata:", response);
-  // });
+
   // unary
   client.sayHello({ name: user }, meta, function(err, response) {
     console.log("Greeting:", response.message);
@@ -47,15 +50,18 @@ function main() {
   })
   stream.write({ name: 'hello 1st client side stream'});
   stream.write({name: 'hello 2nd client side stream' })
+  // ends client streaming
   stream.end({ name: 'hello end client side stream'})
  
   // bidi streaming
   const streamBidi = client.sayHelloBidi(meta);
+  // reads streaming data
   streamBidi.on("error", console.error);
   streamBidi.on("data", console.log);
   streamBidi.on("end", () => client.close());
-
+  // write data
   streamBidi.write ({ name: 'hello 1st bi-di stream'});
+  // ends data
   streamBidi.end({ name: 'hello 2nd bi-di stream'})
 }
 main();
--- grpc_mockData/server.js
+++ grpc_mockData/server.js
@@ -8,16 +8,16 @@ const HOSTPORT = "0.0.0.0:50051";
 
 const dataStream = [
   {
-    message: "Bob"
+    message: "You"
   },
   {
-    message: "Kate"
+    message: "Are"
   },
   {
-    message: "Jim"
+    message: "doing IT"
   },
   {
-    message: "Sara"
+    message: "Champ"
   }
 ];
 
@@ -25,59 +25,70 @@ const dataStream = [
  * Implements the SayHello RPC method.
  */
 
-
+// Unary stream
+// ctx = watch execution context
 function sayHello(ctx) {
+  // create new metadata
   let metadata = new grpc.Metadata();
   metadata.set('it', 'works?')
   metadata.set('indeed', 'it do')
+  // Watcher creates a watch execution context for the watch
+  // The execution context provides scripts and templates with access to the watch metadata
   console.dir(ctx.metadata, { depth: 3, colors: true });
   console.log(`got sayHello request name: ${ctx.req.name}`);
-
-  //setting and sending metadata
-  ctx.sendMetadata({
-    version: 'gRPC v1.24.2',
-    meta_Message: 'metadata sample value'
-  })
-  // console.log(ctx.request.get('meta_Message'))  
+  
+  // an alias to ctx.response.res
+  // This is set only in case of DUPLEX calls, to the the gRPC call reference itself
   ctx.res = { message: "Hello " + ctx.req.name };
-  // let metadata = new grpc.Metadata();
-  // metadata.set('it', 'does?')
-  metadata.set('UNARY', 'yes')
 
+  // send response header metadata object directly as an argument and that is set and sent
+  metadata.set('UNARY', 'yes')
   ctx.sendMetadata(metadata)
+
   console.log(`set sayHello response: ${ctx.res.message}`);
 }
 
+// Server-Side Stream
+// used highland library to manage asynchronous data
 async function sayHellos(ctx) {
+  // create new metadata
   let metadata = new grpc.Metadata();
   metadata.set('it', 'works?')
   metadata.set('indeed', 'it do')
+  // The execution context provides scripts and templates with access to the watch metadata
   console.dir(ctx.metadata, { depth: 3, colors: true });
+  // converts a request into strings
   console.log(`got sayHellos request name:`, JSON.stringify(ctx.req, null, 4));
+
+  // alias for ctx.request.req
+  // In case of UNARY and RESPONSE_STREAM calls it is simply the gRPC call's request
   let reqMessages = {"message": 'hello!!! ' + ctx.req.name}
-  dataStream.push(reqMessages)
-  reqMessages = dataStream
-  console.log('what is this?????', reqMessages)
+ 
   let streamData = await hl(reqMessages)
   ctx.res = streamData;
-  metadata.set('serverStream', 'indeed')
 
+  // send response header metadata object directly as an argument and that is set and sent
+  metadata.set('serverStream', 'indeed')
   ctx.sendMetadata(metadata)
 
   console.log(`done sayHellos`);
+  // ends server stream
   ctx.res.end()
-  // dataStream = [];
 }
 
+// Client-Side stream
 function sayHelloCs (ctx) {
+  // create new metadata
   let metadata = new grpc.Metadata();
   metadata.set('it', 'works?')
   metadata.set('indeed', 'it do')
   metadata.set('clientStream', 'indubitably')
+  // The execution context provides scripts and templates with access to the watch metadata
   console.dir(ctx.metadata, { depth: 3, colors: true })
   console.log('got sayHelloClients')
-  let counter = 0
-  // console.log("ctx content:",ctx.req)
+  let counter = 0;
+  let messages = [];
+  // client streaming calls to write messages and end writing before you can get the response
   return new Promise((resolve, reject) => {
     hl(ctx.req)
       .map(message => {
@@ -85,39 +96,39 @@ function sayHelloCs (ctx) {
         console.log('message content',message.name)
         ctx.response.res = { message: 'Client stream: ' + message.name }
         ctx.sendMetadata(metadata)
-
-
-
       })
+      // returns all the elements as an array
       .collect()
       .toCallback((err, result) => {
         if (err) return reject(err)
         console.log(`done sayHelloClients counter ${counter}`)
-        ctx.response.res = { message: 'SAYHELLOCs Client stream: ' + counter }
+        ctx.response.res = { message: 'SAYHELLOCs Client stream: ' + messages }
         console.log(ctx.response.res)
         resolve()
       })
   })
 }
 
+// Bi-Di stream
 function sayHelloBidi(ctx) {
+  // create new metadata
   let metadata = new grpc.Metadata();
   metadata.set('it', 'works?')
   metadata.set('indeed', 'it do')
   console.log("got sayHelloBidi");
+  // The execution context provides scripts and templates with access to the watch metadata
   console.dir(ctx.metadata, { depth: 3, colors: true });
   let counter = 0;
   ctx.req.on("data", d => {
     counter++;
     ctx.res.write({ message: "bidi stream: " + d.name });
-    
-
   });
   metadata.set('bidiStream', 'ohyes')
   ctx.sendMetadata(metadata);
+  // calls end to client before closing server
   ctx.req.on("end", () => {
     console.log(`done sayHelloBidi counter ${counter}`);
-
+    // ends server stream
     ctx.res.end();
   });
 }
