b7be010d513fedd93ae662b53804bd09a3e2cf1d
h15200 <45977354+h15200@users.noreply.github.com>
2020-06-26 20:06:41 -0700

    start DOM testing

--- test/electronTests.js
+++ test/electronTests.js
@@ -5,6 +5,7 @@ const { Application } = require("spectron");
 const assert = require("assert");
 const electronPath = require("electron");
 const path = require("path");
+const fs = require("fs");
 
 const TEST_MODE = "TEST_MODE";
 
@@ -18,46 +19,56 @@ const app = new Application({
   args: [path.join(__dirname, ".."), TEST_MODE],
 });
 
-describe("Browser Window Tests", function () {
+describe("Electron Tests", function () {
   this.timeout(10000);
   before(function () {
     return app.start();
   });
 
   after(function () {
-    if (app && app.isRunning()) {
-      app.stop();
-    }
+    // if (app && app.isRunning()) {
+    return app.stop();
+    // }
   });
 
-  it("window is visible", async () => {
-    const isVisible = await app.browserWindow.isVisible();
-    assert.equal(isVisible, true);
-  });
+  describe("Browser Window Tests", () => {
+    it("window is visible", async () => {
+      const isVisible = await app.browserWindow.isVisible();
+      return assert.equal(isVisible, true);
+    });
+    it("browser window title is 'Swell'", async () => {
+      const titleWithBrowser = await app.browserWindow.getTitle();
+      return assert.equal(titleWithBrowser, "Swell");
+    });
 
-  it("window title is Swell", async () => {
-    const title = await app.browserWindow.getTitle();
-    assert.equal(title, "Swell");
+    it("Confirm browser window count is 1", async () => {
+      const windowCount = await app.client.getWindowCount();
+      return assert.equal(1, windowCount);
+    });
+    it("take a snapshot of app", async () => {
+      const imageBuffer = await app.browserWindow.capturePage();
+      fs.writeFileSync(path.resolve(__dirname, "snapshot.png"), imageBuffer);
+    });
   });
-  it("Confirm window count is 1", async () => {
-    const windowCount = await app.client.getWindowCount();
-    assert.equal(1, windowCount);
-  });
-  it("devTool should NOT open since we are in production mode", async () => {
-    const isOpen = await app.browserWindow.isDevToolsOpened();
-    assert.equal(false, isOpen);
+  describe("DOM Tests", () => {
+    it("html file title is 'Swell'", async () => {
+      const titleWithClient = await app.client
+        .waitUntilWindowLoaded()
+        .getTitle(); // the dom set inside dist/index.html which is set inside webpack htmlPlugin
+      return assert.equal(titleWithClient, "Swell");
+    });
+
+    it("devTool should NOT open since we are in production mode", async () => {
+      const isOpen = await app.browserWindow.isDevToolsOpened();
+      return assert.equal(isOpen, false);
+    });
+    it("Sidebar exists", async () => {
+      await app.client.waitUntilWindowLoaded();
+      const sidebar = await app.client.$(".sidebar_composer-console");
+      const trash = await app.client.$(".asldkfjlaksdjf");
+      // return assert.equal(sidebar.value)
+      console.log(trash);
+      return assert.equal(trash.value, null);
+    });
   });
 });
-
-// const isDev = await app.browserWindow.isDevToolsOpened();
-// console.log(isDev);
-
-//   it("shows an initial window & 3 dev tools", function () {
-//     // console.log(this.app.client.windowHandles());
-//     console.log(app.client.getTitle());
-//     return app.client.getWindowCount().then(function (count) {
-//       assert.equal(count, 4);
-//       // Please note that getWindowCount() will return 4 if `dev tools` are opened (initial window plus 3 dev tools windows including React & Redux extensions).
-//     });
-//   });
-// });
