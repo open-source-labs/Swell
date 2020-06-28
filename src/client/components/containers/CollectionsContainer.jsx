9cdcc027c4f7cad87072a27f4d62fbcbf7655b38
h15200 <45977354+h15200@users.noreply.github.com>
2020-06-28 16:49:14 -0700

    fix duplicate collections-import not triggering any errors issue

--- src/client/components/containers/CollectionsContainer.jsx
+++ src/client/components/containers/CollectionsContainer.jsx
@@ -1,46 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions/actions';
-import Collection from '../display/Collection.jsx'
-import collectionsController from '../../controllers/collectionsController';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions/actions";
+import Collection from "../display/Collection.jsx";
+import collectionsController from "../../controllers/collectionsController";
 
-const mapStateToProps = store => ({
+const mapStateToProps = (store) => ({
   collections: store.business.collections,
 });
 
-const mapDispatchToProps = dispatch => ({
-  deleteFromCollection: (collection) => { dispatch(actions.deleteFromCollection(collection)) },
-  collectionToReqRes: (reqResArray) => { dispatch(actions.collectionToReqRes(reqResArray)) },
+const mapDispatchToProps = (dispatch) => ({
+  deleteFromCollection: (collection) => {
+    dispatch(actions.deleteFromCollection(collection));
+  },
+  collectionToReqRes: (reqResArray) => {
+    dispatch(actions.collectionToReqRes(reqResArray));
+  },
 });
 
 class CollectionsContainer extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // console.log("props ->", this.props.collections);
+    collectionsController.importCollection(this.props.collections);
   }
 
   render() {
-
-    let collectionComponents = this.props.collections.map((collection, idx) => {
-      return <Collection
-        content={collection} key={idx}
-        deleteFromCollection={this.props.deleteFromCollection}
-        collectionToReqRes={this.props.collectionToReqRes}
-      />
-    })
+    const collectionComponents = this.props.collections.map(
+      (collection, idx) => {
+        return (
+          <Collection
+            content={collection}
+            key={idx}
+            deleteFromCollection={this.props.deleteFromCollection}
+            collectionToReqRes={this.props.collectionToReqRes}
+          />
+        );
+      }
+    );
 
     return (
-      <div className={'collections-container'}>
+      <div className="collections-container">
         <h1>Collections</h1>
         <div className="collection-import-container">
-          <button className="import-collections" onClick={collectionsController.importCollection}>Import Collection</button>
+          <button className="import-collections" onClick={this.handleClick}>
+            Import Collection
+          </button>
         </div>
         {collectionComponents}
       </div>
-    )
+    );
   }
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(CollectionsContainer);
