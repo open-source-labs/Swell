b58fc05d2df92db1d18e18d5bc88878f7b6f2e7c
h15200 <45977354+h15200@users.noreply.github.com>
2020-06-27 19:51:16 -0700

    run prettier on uiReducer.js, ui.js, ComposerWarning.jsx, ComposerContainer.jsx, ComposerNewRequest.jsx

--- __tests__/uiReducer.js
+++ __tests__/uiReducer.js
@@ -1,39 +1,39 @@
-import reducer from '../src/client/reducers/ui'
+import reducer from "../src/client/reducers/ui";
 
-describe('UI Reducer', () => {
+describe("UI Reducer", () => {
   let state;
-  
+
   beforeEach(() => {
     state = {
       warningIsDisplayed: null,
-      composerDisplay: 'Request',
+      composerDisplay: "Request",
     };
-  })
+  });
 
-  describe('SHOW_WARNING', () => {
-    const action = { type: 'SHOW_WARNING'};
+  describe("SHOW_WARNING", () => {
+    const action = { type: "SHOW_WARNING" };
 
-    it('should set warningIsDisplayed to true', () => {
+    it("should set warningIsDisplayed to true", () => {
       const { warningIsDisplayed } = reducer(state, action);
       expect(warningIsDisplayed).toBe(true);
-    })
-  })
+    });
+  });
 
-  describe('HIDE_WARNING', () => {
-    const action = { type: 'HIDE_WARNING'};
+  describe("HIDE_WARNING", () => {
+    const action = { type: "HIDE_WARNING" };
 
-    it('should set warningIsDisplayed to false', () => {
+    it("should set warningIsDisplayed to false", () => {
       const { warningIsDisplayed } = reducer(state, action);
       expect(warningIsDisplayed).toBe(false);
-    })
-  })
+    });
+  });
 
-  describe('SET_COMPOSER_DISPLAY', () => {
-    const action = { type: 'SET_COMPOSER_DISPLAY', payload: 'CHANGE' }
+  describe("SET_COMPOSER_DISPLAY", () => {
+    const action = { type: "SET_COMPOSER_DISPLAY", payload: "CHANGE" };
 
-    it('should update the composerDisplay', () => {
+    it("should update the composerDisplay", () => {
       const { composerDisplay } = reducer(state, action);
       expect(composerDisplay).toEqual(action.payload);
-    })
-  })
-})
+    });
+  });
+});
--- src/client/components/composer/ComposerContainer.jsx
+++ src/client/components/composer/ComposerContainer.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions/actions";
 
-import ComposerNewRequest from './NewRequest/ComposerNewRequest.jsx';
-import ComposerWarning from './Warning/ComposerWarning.jsx';
+import ComposerNewRequest from "./NewRequest/ComposerNewRequest.jsx";
+import ComposerWarning from "./Warning/ComposerWarning.jsx";
 
-const mapStateToProps = store => ({
+const mapStateToProps = (store) => ({
   reqResArray: store.business.reqResArray,
   composerDisplay: store.ui.composerDisplay,
   newRequestFields: store.business.newRequestFields,
@@ -18,16 +18,34 @@ const mapStateToProps = store => ({
   warningMessage: store.business.warningMessage,
 });
 
-const mapDispatchToProps = dispatch => ({
-  reqResAdd: (reqRes) => { dispatch(actions.reqResAdd(reqRes)) },
-  setComposerWarningMessage: (message) => { dispatch(actions.setComposerWarningMessage(message)) },
-  setComposerDisplay: (composerDisplay) => { dispatch(actions.setComposerDisplay(composerDisplay)) },
-  setNewRequestHeaders: (requestHeadersObj) => { dispatch(actions.setNewRequestHeaders(requestHeadersObj)) },
-  setNewRequestStreams: (requestStreamsObj) => { dispatch(actions.setNewRequestStreams(requestStreamsObj)) },
-  setNewRequestFields: (requestFields) => { dispatch(actions.setNewRequestFields(requestFields)) },
-  setNewRequestBody: (requestBodyObj) => { dispatch(actions.setNewRequestBody(requestBodyObj)) },
-  setNewRequestCookies: (requestCookiesObj) => { dispatch(actions.setNewRequestCookies(requestCookiesObj)) },
-  setNewRequestSSE: (requestSSEBool) => { dispatch(actions.setNewRequestSSE(requestSSEBool)) }
+const mapDispatchToProps = (dispatch) => ({
+  reqResAdd: (reqRes) => {
+    dispatch(actions.reqResAdd(reqRes));
+  },
+  setComposerWarningMessage: (message) => {
+    dispatch(actions.setComposerWarningMessage(message));
+  },
+  setComposerDisplay: (composerDisplay) => {
+    dispatch(actions.setComposerDisplay(composerDisplay));
+  },
+  setNewRequestHeaders: (requestHeadersObj) => {
+    dispatch(actions.setNewRequestHeaders(requestHeadersObj));
+  },
+  setNewRequestStreams: (requestStreamsObj) => {
+    dispatch(actions.setNewRequestStreams(requestStreamsObj));
+  },
+  setNewRequestFields: (requestFields) => {
+    dispatch(actions.setNewRequestFields(requestFields));
+  },
+  setNewRequestBody: (requestBodyObj) => {
+    dispatch(actions.setNewRequestBody(requestBodyObj));
+  },
+  setNewRequestCookies: (requestCookiesObj) => {
+    dispatch(actions.setNewRequestCookies(requestCookiesObj));
+  },
+  setNewRequestSSE: (requestSSEBool) => {
+    dispatch(actions.setNewRequestSSE(requestSSEBool));
+  },
 });
 
 class ComposerContainer extends Component {
@@ -40,10 +58,10 @@ class ComposerContainer extends Component {
     // this.setState({
     //   composerDisplay: this.props.composerDisplay,
     // });
-
   }
 
-  componentDidUpdate() {//keeping the redux store state in sync with this component's local state
+  componentDidUpdate() {
+    //keeping the redux store state in sync with this component's local state
     // if (this.props.composerDisplay !== this.state.composerDisplay) {
     //   this.setState({
     //     composerDisplay: this.props.composerDisplay,
@@ -53,46 +71,47 @@ class ComposerContainer extends Component {
 
   render() {
     let composerContents;
-    switch (this.props.composerDisplay) { // conditional rendering of components based on the value of composerDisplay in redux store
-      case 'Request': {
-        composerContents = <ComposerNewRequest
-          composerDisplay={this.props.composerDisplay}
-          newRequestFields={this.props.newRequestFields}
-          newRequestHeaders={this.props.newRequestHeaders}
-          newRequestStreams={this.props.newRequestStreams}
-          newRequestCookies={this.props.newRequestCookies}
-          newRequestBody={this.props.newRequestBody}
-          newRequestSSE={this.props.newRequestSSE}
-          currentTab={this.props.currentTab}
-          reqResAdd={this.props.reqResAdd}
-
-          setComposerWarningMessage={this.props.setComposerWarningMessage}
-          setComposerDisplay={this.props.setComposerDisplay}
-
-          setNewRequestFields={this.props.setNewRequestFields}
-          setNewRequestHeaders={this.props.setNewRequestHeaders}
-          setNewRequestStreams={this.props.setNewRequestStreams}
-          setNewRequestCookies={this.props.setNewRequestCookies}
-          setNewRequestBody={this.props.setNewRequestBody}
-          setNewRequestSSE={this.props.setNewRequestSSE}
-        />;
+    switch (
+      this.props.composerDisplay // conditional rendering of components based on the value of composerDisplay in redux store
+    ) {
+      case "Request": {
+        composerContents = (
+          <ComposerNewRequest
+            composerDisplay={this.props.composerDisplay}
+            newRequestFields={this.props.newRequestFields}
+            newRequestHeaders={this.props.newRequestHeaders}
+            newRequestStreams={this.props.newRequestStreams}
+            newRequestCookies={this.props.newRequestCookies}
+            newRequestBody={this.props.newRequestBody}
+            newRequestSSE={this.props.newRequestSSE}
+            currentTab={this.props.currentTab}
+            reqResAdd={this.props.reqResAdd}
+            setComposerWarningMessage={this.props.setComposerWarningMessage}
+            setComposerDisplay={this.props.setComposerDisplay}
+            setNewRequestFields={this.props.setNewRequestFields}
+            setNewRequestHeaders={this.props.setNewRequestHeaders}
+            setNewRequestStreams={this.props.setNewRequestStreams}
+            setNewRequestCookies={this.props.setNewRequestCookies}
+            setNewRequestBody={this.props.setNewRequestBody}
+            setNewRequestSSE={this.props.setNewRequestSSE}
+          />
+        );
         break;
       }
-      case 'Warning': {
-        composerContents = <ComposerWarning
-          warningMessage={this.props.warningMessage}
-          setComposerDisplay={this.props.setComposerDisplay}
-        />;
+      case "Warning": {
+        composerContents = (
+          <ComposerWarning
+            warningMessage={this.props.warningMessage}
+            setComposerDisplay={this.props.setComposerDisplay}
+          />
+        );
         break;
       }
       default:
-        console.log('Incorrect Model Display setting');
+        console.log("Incorrect Model Display setting");
     }
     return <div className="composerContents">{composerContents}</div>;
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ComposerContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ComposerContainer);
