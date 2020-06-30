98bbb961bb5552436955c2d7745979c53cda6ea5
Grace Spletzer <gracespletzer05@gmail.com>
2020-06-30 16:59:13 -0700

    test update

--- __tests__/test-utils.js
+++ __tests__/test-utils.js
@@ -3,12 +3,61 @@ import React from "react";
 import { render as rtlRender } from "@testing-library/react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import format from "date-fns/format";
 import reducers from "../src/client/reducers/index.js";
+
+let today = new Date();
+//generate date for yesterday
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+//generate date for two days prior
+let twoDaysAgo = new Date();
+twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
 
 const initialState = {
   currentTab: "First Tab",
   reqResArray: [],
-  history: [],
+  history: [
+    {
+      date: format(today, "MM/DD/YYYY"),
+      history: [
+        {
+          id: 1,
+          created_at: today,
+          url: "http://google.com",
+          request: {
+            method: "GET",
+          },
+        },
+      ],
+    },
+    {
+      date: format(yesterday, "MM/DD/YYYY"),
+      history: [
+        {
+          id: 2,
+          created_at: yesterday,
+          url: "http://facebook.com",
+          request: {
+            method: "GET",
+          },
+        },
+      ],
+    },
+    {
+      date: format(twoDaysAgo, "MM/DD/YYYY"),
+      history: [
+        {
+          id: 3,
+          created_at: twoDaysAgo,
+          url: "http://instagram.com",
+          request: {
+            method: "GET",
+          },
+        },
+      ],
+    },
+  ],
   collections: [],
   warningMessage: "",
   newRequestFields: {
--- src/client/components/display/HistoryDate.jsx
+++ src/client/components/display/HistoryDate.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import History from './History.jsx';
-import parse from 'date-fns/parse';
-import isYesterday from 'date-fns/is_yesterday';
-import isToday from 'date-fns/is_today';
-import format from 'date-fns/format';
+import React, { Component } from "react";
+import History from "./History.jsx";
+import parse from "date-fns/parse";
+import isYesterday from "date-fns/is_yesterday";
+import isToday from "date-fns/is_today";
+import format from "date-fns/format";
 
 class HistoryDate extends Component {
   constructor(props) {
@@ -13,37 +13,47 @@ class HistoryDate extends Component {
   }
 
   focusOnForm(event) {
-    let composerUrlField = document.querySelector('.composer_url_input');
-    composerUrlField.focus()
+    let composerUrlField = document.querySelector(".composer_url_input");
+    composerUrlField.focus();
   }
 
   render() {
-    let current = this.props.history.find(a => a.date === this.props.content.date);
+    let current = this.props.history.find(
+      (a) => a.date === this.props.content.date
+    );
     let date = parse(current.date);
 
-    if (isToday(date)) { date = 'Today' } // If the date matches todays date render the word "Today"
-    else if (isYesterday(date)) { date = 'Yesterday' }
-    else { date = format(date, 'ddd, MMM D, YYYY') }
+    if (isToday(date)) {
+      date = "Today";
+    } // If the date matches todays date render the word "Today"
+    else if (isYesterday(date)) {
+      date = "Yesterday";
+    } else {
+      date = format(date, "ddd, MMM D, YYYY");
+    }
 
     let histArray = current.history.map((history, i) => {
-      return <History
-        content={history} key={i}
-        focusOnForm={this.focusOnForm}
-        deleteFromHistory={this.props.deleteFromHistory}
-        setNewRequestFields={this.props.setNewRequestFields}
-        setNewRequestHeaders={this.props.setNewRequestHeaders}
-        setNewRequestCookies={this.props.setNewRequestCookies}
-        setNewRequestBody={this.props.setNewRequestBody}
-        setNewRequestStreams={this.props.setNewRequestStreams}
-      />
-    })
+      return (
+        <History
+          content={history}
+          key={i}
+          focusOnForm={this.focusOnForm}
+          deleteFromHistory={this.props.deleteFromHistory}
+          setNewRequestFields={this.props.setNewRequestFields}
+          setNewRequestHeaders={this.props.setNewRequestHeaders}
+          setNewRequestCookies={this.props.setNewRequestCookies}
+          setNewRequestBody={this.props.setNewRequestBody}
+          setNewRequestStreams={this.props.setNewRequestStreams}
+        />
+      );
+    });
 
     return (
-      <div className={'historyDate'}>
-        <h1>{date}</h1>
+      <div className="historyDate">
+        <h1 role="queryDate">{date}</h1>
         {histArray}
       </div>
-    )
+    );
   }
 }
 
