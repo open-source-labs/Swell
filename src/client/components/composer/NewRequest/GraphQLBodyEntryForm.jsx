868b23b3b27a8b5439e0710221ae64144754356a
mjmiguel <migueljmiller@gmail.com>
2020-08-12 12:33:00 -0700

    add cm and path for webpack css

--- src/client/components/composer/NewRequest/GraphQLBodyEntryForm.jsx
+++ src/client/components/composer/NewRequest/GraphQLBodyEntryForm.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror-graphql/mode';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/theme/monokai.css';
+
+
 import GraphQLVariableEntryForm from './GraphQLVariableEntryForm.jsx';
 import dropDownArrow from '../../../../assets/icons/arrow_drop_down_white_192x192.png'
 
@@ -42,27 +49,34 @@ class GraphQLBodyEntryForm extends Component {
     return (
       <div >
         <div className='composer_subtitle' onClick={this.toggleShow} style={this.props.stylesObj}>
-          <img className={arrowClass} src={dropDownArrow}>
-          </img>
+          <img className={arrowClass} src={dropDownArrow} />
           Body
         </div>
-
-        <textarea
+        <div  id='gqlBodyEntryTextArea'>
+        <CodeMirror 
           value={this.props.newRequestBody.bodyContent}
-          className={'composer_textarea gql ' + bodyContainerClass}
-          id='gqlBodyEntryTextArea'
+          options={{
+            mode: 'graphql',
+            theme: 'monokai'
+          }}
+          // width="500px"
+          height="18vh"
+
           style={{ 'resize': 'none' }} //tried making top-margin/topMargin -10px but it didn't care
           type='text'
           placeholder='Body'
           rows={10}
           onKeyDown={(e) => this.handleKeyPress(e)}
-          onChange={(e) => {
-            this.props.setNewRequestBody({
-              ...this.props.newRequestBody,
-              bodyContent: e.target.value
-            })
-          }}
-        ></textarea>
+          // onChange={(e) => {
+          //   this.props.setNewRequestBody({
+          //     ...this.props.newRequestBody,
+          //     bodyContent: e.target.value
+          //   })
+          // }}
+        />
+
+        </div>
+
         <GraphQLVariableEntryForm
           newRequestBody={this.props.newRequestBody}
           setNewRequestBody={this.props.setNewRequestBody}
