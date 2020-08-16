ec81820364df2de8d7521f6f40732d7054d709da
Alex Sanhueza <sanhuezainc@gmail.com>
2020-08-16 10:58:16 -0700

    converted GQL introspection log to functional component w hooks

--- src/client/components/composer/NewRequest/GraphQLIntrospectionLog.jsx
+++ src/client/components/composer/NewRequest/GraphQLIntrospectionLog.jsx
@@ -1,65 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import dropDownArrow from "../../../../assets/icons/arrow_drop_down_white_192x192.png";
 import graphQLController from "../../../controllers/graphQLController";
 
-import { render } from "react-dom";
+const GraphQLIntrospectionLog = (props) => {
+  const [show, toggleShow] = useState(false);
 
-class GraphQLIntrospectionLog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-    };
-    this.toggleShow = this.toggleShow.bind(this);
-  }
-  toggleShow() {
-    this.setState({
-      show: !this.state.show,
-    });
-  }
+  const { introspectionData, url } = props;
+  const arrowClass = show
+    ? "composer_subtitle_arrow-open"
+    : "composer_subtitle_arrow-closed";
+  const bodyContainerClass = show
+    ? "composer_bodyform_container-open"
+    : "composer_bodyform_container-closed";
+  const logAreaClass =
+    introspectionData &&
+    introspectionData !== "Error: Please enter a valid GraphQL API URI"
+      ? "introspection-big"
+      : "introspection-small";
 
-  render() {
-    const { introspectionData, url } = this.props;
-    const arrowClass = this.state.show
-      ? "composer_subtitle_arrow-open"
-      : "composer_subtitle_arrow-closed";
-    const bodyContainerClass = this.state.show
-      ? "composer_bodyform_container-open"
-      : "composer_bodyform_container-closed";
-    const logAreaClass =
-      introspectionData &&
-      introspectionData !== "Error: Please enter a valid GraphQL API URI"
-        ? "introspection-big"
-        : "introspection-small";
-
-    return (
-      <div>
-        <div
-          className="composer_subtitle"
-          onClick={this.toggleShow}
-          style={this.props.stylesObj}
-        >
-          <img className={arrowClass} src={dropDownArrow}></img>
-          Schema
-        </div>
-        <div className={bodyContainerClass}>
-          <textarea
-            readOnly
-            className={`composer_textarea gql introspection-small ${logAreaClass}`}
-            value={
-              introspectionData || 'Click "Introspect" to view GraphQL Schema'
-            }
-          />
-          <button
-            className="composer_submit gql"
-            onClick={() => graphQLController.introspect(url)}
-          >
-            Introspect
-          </button>
-        </div>
+  return (
+    <div>
+      <div
+        className="composer_subtitle"
+        onClick={() => toggleShow((show) => !show)}
+        style={props.stylesObj}
+      >
+        <img className={arrowClass} src={dropDownArrow}></img>
+        Schema
       </div>
-    );
-  }
-}
+      <div className={bodyContainerClass}>
+        <textarea
+          readOnly
+          className={`composer_textarea gql introspection-small ${logAreaClass}`}
+          value={
+            introspectionData || 'Click "Introspect" to view GraphQL Schema'
+          }
+        />
+        <button
+          className="composer_submit gql"
+          onClick={() => graphQLController.introspect(url)}
+        >
+          Introspect
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default GraphQLIntrospectionLog;
