98d1b95860597305a3599d58fb605d8dab062b68
Alex Sanhueza <sanhuezainc@gmail.com>
2020-08-18 17:17:41 -0700

    BAR GRAPH REACT COMPONENT WORKS

--- src/client/components/containers/GraphContainer.tsx
+++ src/client/components/containers/GraphContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Graph from "../display/Graph.jsx";
-import BarGraph from "../display/BarGraph.jsx";
+import BarGraph from "../display/BarGraph2.jsx";
 
 export class GraphContainer extends React.Component<any, any> {
   constructor(props: any) {
--- src/client/components/display/BarGraph2.jsx
+++ src/client/components/display/BarGraph2.jsx
@@ -10,44 +10,81 @@ const mapStateToProps = (store) => ({
 
 const BarGraph = (props) => {
   const [dataPoints, updateData] = useState(props.dataPoints);
-  console.log(dataPoints);
+  const [chartData, updateChart] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Response (ms)",
+        data: [],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  });
 
-  let times;
+  const dataUpdater = (labelArr, dataArr) => {
+    return {
+      labels: labelArr,
+      datasets: [
+        {
+          label: "Response (ms)",
+          data: dataArr,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
+
+  useEffect(() => updateData(props.dataPoints));
 
   useEffect(() => {
-    updateData(props.dataPoints);
-    times = dataPoints.map((point) => point.timeReceived - point.timeSent);
-    console.log(times);
-  });
+    const urls = dataPoints.length ? dataPoints.map((point) => point.url) : [];
+    const times = props.dataPoints.length
+      ? dataPoints.map((point) => point.timeReceived - point.timeSent)
+      : [];
+    const updatedChart = dataUpdater(urls, times);
+    console.log(urls.length, "  ", chartData.labels.length);
+    if (chartData.labels.length !== urls.length) {
+      console.log(chartData.labels);
+      updateChart(updatedChart);
+    }
+  }, [dataPoints]);
 
   return (
     <div id="chartContainer" className="chart">
       <Bar
-        data={{
-          datasets: [
-            {
-              label: "Response (ms)",
-              data: times,
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        }}
+        data={chartData}
         width={50}
         height={100}
         options={{
