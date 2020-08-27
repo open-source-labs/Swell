7678472d2d859db1c02bc3832a5ac0b7dbb821c1
Grace Kim <grace@Graces-MacBook-Pro.local>
2020-08-27 14:37:50 -0700

    wyatts changes

--- test/subSuites/graphqlTest.js
+++ test/subSuites/graphqlTest.js
@@ -3,17 +3,38 @@ const sideBar = require("../pageObjects/Sidebar.js");
 const reqRes = require("../pageObjects/ReqRes.js");
 
 // schema introspection works
-// queries work
-/* example query:
-url: https://countries.trevorblades.com
-query {
-  Country(code: "AE") {
-    capital{name}
-  }
-}
-*/
 // can use schema-hinter for queries
+// queries work
+  /* example public API query:
+  url: https://countries.trevorblades.com
+  query {
+    Country(code: "AE") {
+      capital{name}
+    }
+  }
+  */
+   /* example local API query:
+  url: http://localhost:4000/graphql
+  query {
+    feed {
+      id
+      url
+      description
+    }
+  }
+  */
 // mutations work
+  /* example local API mutation:
+  url: http://localhost:4000/graphql
+  mutation {
+    post(
+      url: "www.piedpiper.com"
+      description: "Middle-out compression"
+    ) {
+      url
+    }
+  }
+  */
 // subscriptions work
-// entering wrong query will give you the appropriate error message
+// entering wrong query/mutation will give you the appropriate error message
 // can use variables
