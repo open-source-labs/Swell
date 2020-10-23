7c0b164dc119480616d2b68665e9e035e69e1694
garyslootskiy <69308215+garyslootskiy@users.noreply.github.com>
2020-10-23 16:02:19 -0400

    Populate cookie and header request details in request cards

--- src/client/components/display/RestRequestContent.jsx
+++ src/client/components/display/RestRequestContent.jsx
@@ -1,15 +1,56 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 
+import ContentReqRow from './ContentReqRow';
+
 export default function RestRequestContent({ request }) {
   // const [showModal, setShowModal] = useState(false);
   // const dispatch = useDispatch();
   // PULL elements FROM store
   // const content = useSelector(store => store.business.content);
-  console.log(request);
+  console.log('RestRequestContent Props:',request);
+  const { 
+    method, // "POST"
+    headers, // [{id: 0, active: true, key: 'key', value: 'value'}]
+    cookies, // [{id: 0, active: true, key: 'key', value: 'value'}]
+    body, // "body Content text"
+    bodyType, // "raw"
+    bodyVariables, // ""
+    rawType, // "Text (text/plain)"
+    isSSE, // false/true
+    network, // "rest"
+    restUrl, // "http://sdfgsdfgdsfg"
+    wsUrl, // "ws://"
+    gqlUrl, // "https://"
+    grcpUrl // ""
+  } = request;
+
+  // CREATE HEADER COMPONENTS
+  const headerRows = headers.map((header, index) => <ContentReqRow data={header} key={`h${index}`}/>);
+
+  // CREATE COOKIE COMPONENTS
+  const cookieRows = cookies.map((cookie, index) => <ContentReqRow data={cookie} key={`h${index}`}/>);
+
   return (
     <div>
-      body
+      {/* REQUEST DETAILS */}
+      <div className="p-3">
+        {/* HEADERS */}
+        <div>Headers</div>
+        {headerRows}
+        {/* COOKIES */}
+        <div>Cookies</div>
+        {cookieRows}
+        {/* BODY */}
+        <div>Body</div>
+        {/* FIGURE OUT CODEMIRROR!!!!!!!!!!!!!!!!!! */}
+      </div>
+      {/* BUTTONS */}
+      <div className="columns">
+        <div className="column">Remove</div>
+        <div className="column">Send</div>
+      </div>
+
     </div>
   )
 }
