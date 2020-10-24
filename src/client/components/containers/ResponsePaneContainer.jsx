12fc1d4342029fad07991d2eccf32037cb5ec43c
Robin Yoong <pulpandmills@gmail.com>
2020-10-24 19:16:39 +0800

    Add header tabs on Response pane

--- src/client/components/containers/ResponsePaneContainer.jsx
+++ src/client/components/containers/ResponsePaneContainer.jsx
@@ -1,17 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
-export class ResponsePaneContainer extends Component {
-  render() {
-    return (
+export const ResponsePaneContainer = () => {
+  const [activeTab, setActiveTab] = useState('workspace');
+
+  return (
       <div className='column is-one-third'>
         {/* HEADER */}
         <div className="hero is-primary">
           <h3>Responses</h3>
         </div>
-        **ResponsePaneContainer**
+        {/* TAB SELECTOR */}
+        <div className="tabs">
+          <ul>
+            <li className={activeTab === 'workspace' ? 'is-active' : ''}>
+              <a 
+                onClick={() => setActiveTab('workspace')}
+              >Requests
+              </a>
+            </li>
+            <li className={activeTab === 'saved-workspace' ? 'is-active' : ''}>
+              <a 
+                onClick={() => setActiveTab('saved-workspace')}
+              >Saved Workspace
+              </a>
+            </li>
+          </ul>
+        </div>
+
       </div>
-    )
-  }
+  )
+
 }
 
 export default ResponsePaneContainer
