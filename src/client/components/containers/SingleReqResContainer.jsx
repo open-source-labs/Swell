bff119a06d018fe90cca8829a2184ee7b4e0b7f7
garyslootskiy <69308215+garyslootskiy@users.noreply.github.com>
2020-10-29 09:05:27 -0400

    Create copy to Composer button

--- src/client/components/containers/SingleReqResContainer.jsx
+++ src/client/components/containers/SingleReqResContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import * as actions from "../../../../src/client/actions/actions.js";
 
 import connectionController from "../../controllers/reqResController";
@@ -11,6 +12,14 @@ import ReqResCtrl from "../../controllers/reqResController";
 const SingleReqResContainer = (props) => {
   const [showDetails, setShowDetails] = useState(false);
   const dispatch = useDispatch();
+
+  const newRequestFields = useSelector(store => store.business.newRequestFields); 
+  const newRequestStreams = useSelector(store => store.business.newRequestStreams); 
+
+  const routerHistory = useHistory();
+  const routeChange = (path) => { 
+    routerHistory.push(path);
+  }
 
   const {
     content,
@@ -33,7 +42,127 @@ const SingleReqResContainer = (props) => {
     reqResUpdate,
     reqResDelete,
   } = props;
-  const network = content.request.network;
+  const network = content.request.network;  
+
+  const addHistoryToNewRequest = () => {
+    
+    let requestFieldObj = {};
+    if (network === 'rest') {
+      routeChange('/composer/rest');
+      requestFieldObj = {
+        ...newRequestFields,
+        method: content.request.method || 'GET',
+        protocol: content.protocol || 'http://',
+        url: content.url,
+        restUrl: content.request.restUrl,
+        graphQL: content.graphQL || false,
+        gRPC: content.gRPC || false,
+        network,
+      }
+    };
+    if (network === 'ws') {
+      routeChange('/composer/ws');
+      requestFieldObj = {
+        ...newRequestFields,
+        method: content.request.method  || 'GET',
+        protocol: content.protocol  || 'http://',
+        url: content.url,
+        wsUrl: content.request.wsUrl,
+        graphQL: content.graphQL || false,
+        gRPC: content.gRPC || false,
+        network,
+      }
+    };
+    if (network === 'graphQL') {
+      routeChange('/composer/graphql');
+      requestFieldObj = {
+        ...newRequestFields,
+        method: content.request.method || 'GET',
+        protocol: content.protocol || 'http://',
+        url: content.url,
+        gqlUrl: content.request.gqlUrl,
+        graphQL: content.graphQL || false,
+        gRPC: content.gRPC || false,
+        network,
+      }
+    };
+    if (network === 'grpc') {
+      routeChange('/composer/grpc');
+      requestFieldObj = {
+        ...newRequestFields,
+        method: content.request.method || 'GET',
+        protocol: content.protocol || 'http://',
+        url: content.url,
+        grpcUrl: content.request.grpcUrl,
+        graphQL: content.graphQL || false,
+        gRPC: content.gRPC || false,
+        network,
+      }
+    };
+    let headerDeeperCopy;
+    if (content.request.headers) {
+      headerDeeperCopy = JSON.parse(JSON.stringify(content.request.headers));
+      headerDeeperCopy.push({
+        id: content.request.headers.length + 1,
+        active: false,
+        key: '',
+        value: '',
+      })
+    }
+    let cookieDeeperCopy;
+    if (content.request.cookies && !/ws/.test(protocol)) {
+      cookieDeeperCopy = JSON.parse(JSON.stringify(content.request.cookies));
+      cookieDeeperCopy.push({
+        id: content.request.cookies.length + 1,
+        active: false,
+        key: '',
+        value: '',
+      })
+    }
+    const requestHeadersObj = {
+      headersArr: headerDeeperCopy || [],
+      count: headerDeeperCopy ? headerDeeperCopy.length : 1,
+    }
+    const requestCookiesObj = {
+      cookiesArr: cookieDeeperCopy || [],
+      count: cookieDeeperCopy ? cookieDeeperCopy.length : 1,
+    }
+    const requestBodyObj = {
+      bodyType: content.request.bodyType || 'raw',
+      bodyContent: content.request.body || '',
+      bodyVariables: content.request.bodyVariables || '',
+      rawType: content.request.rawType || 'Text (text/plain)',
+      JSONFormatted: true,
+      bodyIsNew: false,
+    }
+    dispatch(actions.setNewRequestFields(requestFieldObj));
+    dispatch(actions.setNewRequestHeaders(requestHeadersObj));
+    dispatch(actions.setNewRequestCookies(requestCookiesObj));
+    dispatch(actions.setNewRequestBody(requestBodyObj));
+
+    // // for gRPC ===> NEED TO FILL OUT
+    // if (content && content.gRPC) {
+    //   const streamsDeepCopy = JSON.parse(JSON.stringify(content.streamsArr));
+    //   const contentsDeepCopy = JSON.parse(JSON.stringify(content.streamContent));
+    //   // construct the streams obj from passed in history content & set state in store
+    //   const requestStreamsObj = {
+    //     streamsArr: streamsDeepCopy,
+    //     count: queryArr.length,
+    //     streamContent: contentsDeepCopy,
+    //     selectedPackage: content.selectedPackage,
+    //     selectedRequest: content.selectedRequest,
+    //     selectedService:  content.selectedService,
+    //     selectedStreamingType: content.selectedStreamingType,
+    //     initialQuery: content.initialQuery,
+    //     queryArr: content.queryArr,
+    //     protoPath: content.protoPath,
+    //     services: content.services,
+    //     protoContent: content.protoContent,
+    //   }
+    //   dispatch(actions.setNewRequestStreams(requestStreamsObj))
+    // }
+    // setSidebarTab('composer');
+  }
 
   const removeReqRes = () => {
     connectionController.closeReqRes(id);
@@ -49,11 +178,25 @@ const SingleReqResContainer = (props) => {
       </div>
       {/* VIEW REQUEST DETAILS / MINIMIZE */}
       {network !== 'ws' &&
-        <div className='is-neutral-300 is-size-7 cards-dropdown minimize-card pl-2 is-flex is-align-items-center' 
+        <div className='is-neutral-300 is-size-7 cards-dropdown minimize-card pl-2 is-flex is-align-items-center is-justify-content-space-between' 
           onClick={() => { setShowDetails(showDetails === false)}}
           >
-          View Request Details
+          {showDetails === true &&
+            "Hide Request Details"
+          }
+          {showDetails === false &&
+            "View Request Details"
+          }
+          {showDetails === true &&
+          <div 
+            className="is-clickable is-primary-link m-3" 
+            onClick={addHistoryToNewRequest}
+          >
+            Copy to Composer
+          </div>
+          }
         </div>
+        
       }
       {/* REQUEST ELEMENTS */}
       {showDetails === true &&
