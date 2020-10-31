59a628df91af7197913f4f24e50f666a8907b496
Sam Haar <samhaar@gmail.com>
2020-10-31 12:15:18 -0400

    change main app behavior to quit when window is closed

--- main.js
+++ main.js
@@ -203,12 +203,7 @@ app.on("ready", () => {
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    //darwin refers to macOS...
-    app.quit(); // If User is on mac exit the program when all windows are closed
-  }
+  app.quit();
 });
 
 // Auto Updating Functionality
