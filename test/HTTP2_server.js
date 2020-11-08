00a6cb75fb7943be03467486a9ea988f00faf7d9
Sam Haar <samhaar@gmail.com>
2020-11-08 11:36:55 -0500

    improves http2 mock server to better deal with sse streams and client side stream closing

--- test/HTTP2_server.js
+++ test/HTTP2_server.js
@@ -3,25 +3,21 @@ const fs = require('fs');
 const CERT_PATH = 'test/HTTP2_cert.pem';
 const PRIV_PATH = 'test/HTTP2_private.pem';
 
-let count = 0;
 
 const server = http2.createSecureServer({
   cert: fs.readFileSync(CERT_PATH),
   key: fs.readFileSync(PRIV_PATH),
 });
+
 server.on('error', (err) => console.error(err));
 
 server.on('stream', (stream, headers) => {
-
-  // respond with stream if request accepts stream in headers
+  // respond with SSE stream if request accepts stream in headers
   if (headers.accept && headers.accept.includes('stream')) {
-    stream.respond({
-      'content-type': 'text/event-stream; charset=utf-8',
-      ':status': 200
-    });
-    return go(stream);
+    sendStreamToClient(stream);
+    return;
   }
-  
+
   // else send a single event
   stream.respond({
     'content-type': 'application/json; charset=utf-8',
@@ -30,15 +26,39 @@ server.on('stream', (stream, headers) => {
   stream.end(JSON.stringify({data: 'hello and goodbye'}));
 });
 
-const go = (stream) => {
-  if (++count > 10) {
-    stream.end(`id: ${count}\nevent: testMessage\ndata: goodbye\n\n`);
-    count = 0;
-    return;
-  }
+const sendStreamToClient = (stream) => {
+  const STREAM_INTERVAL = 500;
+  let count = 0;
+  let streamIsOpen = true;
   
-  stream.write(`id: ${count}\nevent: testMessage\ndata: hello\n\n`);
-  setTimeout(() => go(stream), 500);
+  stream.on('close', () => {
+    streamIsOpen = false;
+    console.log('stream closed');
+  });
+
+  stream.respond({
+    'content-type': 'text/event-stream; charset=utf-8',
+    ':status': 200
+  });
+
+  const sendEvent = (stream) => {
+    if (!streamIsOpen){
+      count = 0;
+      return;
+    }
+    count += 1;
+    if (count < 50) {
+      stream.write(`id: ${count}\nevent: testMessage\ndata: hello\n\n`);
+      setTimeout(() => sendEvent(stream), STREAM_INTERVAL);
+    } else {
+      stream.end(`id: ${count}\nevent: testMessage\ndata: goodbye\n\n`);
+      streamIsOpen = false;
+      count = 0;
+    }
+  };
+  
+  sendEvent(stream);
 }
+
 
 server.listen(8443, () => console.log('server up on 8443'));
