d844eefea98d0ece03e8cc5260772f83707f3ea9
Edward Cho <edwardcho1231@gmail.com>
2020-12-10 14:05:31 -0500

    removed input field for temporary testing in workspace pane

--- src/client/components/containers/ContentsContainer.tsx
+++ src/client/components/containers/ContentsContainer.tsx
@@ -4,8 +4,6 @@ import * as actions from '../../actions/actions';
 import BarGraph from "../display/BarGraph"
 import WorkspaceContainer from "./WorkspaceContainer.jsx";
 import CollectionsContainer from "./CollectionsContainer";
-import {Controlled as CodeMirror} from 'react-codemirror2';
-
 
 export const ContentsContainer = () => {
   // const [activeTab, setActiveTab] = useState('workspace');
@@ -13,30 +11,8 @@ export const ContentsContainer = () => {
   const activeTab = useSelector(store => store.ui.workspaceActiveTab);
   const currentResponse = useSelector(store => store.business.currentResponse);
   const setActiveTab = (tabName) => dispatch(actions.setWorkspaceActiveTab(tabName));
-  const [testInput, setTestInput] = useState("assert.strictEqual(3, '3', 'no coerscion') expect(2).to.equal(2) assert.strictEqual(3, '3', 'no coerscion')");
 
   const [showGraph, setShowGraph] = useState(false);
-  const {api} = window;
-
-  useEffect(()=> {
-    api.receive('testResult', (result) => {
-      const res = JSON.parse(result);
-      if(!res.message) {
-        console.log('Test passed!')
-      } else {
-        console.log(res.message)
-      }
-    })
-  }, [])
-
-  const click = async () => {
-    try{
-      api.send('testFileSent', testInput)
-    }
-    catch(e) {
-      console.log(e);
-    }
-  }
 
   return (
     <div className="column is-one-third is-flex is-flex-direction-column is-tall is-divider-neutral-300" id='workspace'>
@@ -63,23 +39,7 @@ export const ContentsContainer = () => {
         </ul>
       </div>
       {/* <input style={{height: '100px'}} id="testInput" type="text"></input> */}
-      <CodeMirror
-      value={testInput}
-      onBeforeChange={(editor,data,value) => setTestInput(value)}
-        options={{
-          mode: {
-            name: "javascript",
-            json: true,
-          },
-          json: true,
-          lineNumbers: true,
-          tabSize: 4,
-          lineWrapping: true,
-          pollInterval: 2000,
-          readOnly: false,
-        }}
-      />
-      <button onClick={click}>Click</button>
+      
       {/* WORKSPACE CONTENT */}
       <div className="is-flex-grow-3 add-vertical-scroll">
 
