5313dac3e3b62b39b0ac3696eed3f3ebf4a4574c
jasonligg <9956289+jasonligg@users.noreply.github.com>
2020-12-14 22:13:22 -0500

    created testing subsuite for httpTestingController; used httpTest.js as model; created first test to test for a simple assertion

--- test/subSuites/httpTestingTest.js
+++ test/subSuites/httpTestingTest.js
@@ -1,11 +1,117 @@
 const chai = require('chai');
-
+const chaiHttp = require("chai-http");
 const app = require('../testApp.js');
 const composerObj = require('../pageObjects/ComposerObj');
 const workspaceObj = require('../pageObjects/WorkspaceObj');
 
+const { expect } = chai;
+
 module.exports = () => {
   describe("HTTP Testing Controller", function() {
-    
+    // code below is from httpTest.js file
+    const fillRestRequest = async (url, method, body = '', headers = [], cookies = []) => {
+      try {
+        // click and check REST
+        await composerObj.selectedNetwork.click();
+        await app.client.$('a=REST').click();
+        
+        // click and select METHOD if it isn't GET
+        if(method !== 'GET') {
+          await app.client.$('span=GET').click();
+          await app.client.$(`a=${method}`).click();
+        }
+
+        // type in url
+        await composerObj.url.setValue(url);
+
+        // set headers
+        headers.forEach(async ({ key, value },index) => {
+          await app.client.$(`//*[@id="header-row${index}"]/input[1]`).setValue(key);
+          await app.client.$(`//*[@id="header-row${index}"]/input[2]`).setValue(value);
+          await app.client.$('button=+ Header').click();
+        });
+
+        // set cookies
+        cookies.forEach(async ({ key, value },index) => {
+          await app.client.$(`//*[@id="cookie-row${index}"]/input[1]`).setValue(key);
+          await app.client.$(`//*[@id="cookie-row${index}"]/input[2]`).setValue(value);
+          await app.client.$('button=+ Cookie').click();
+        });
+
+        // Add BODY as JSON if it isn't GET
+        if (method !== "GET") {
+          // select body type JSON
+          await app.client.$('span=text/plain').click();
+          await app.client.$('a=application/json').click();
+          // insert JSON content into body
+          await composerObj.clearRestBodyAndWriteKeys(body);
+        }
+      } catch(err) {
+        console.error(err)
+      }
+    };
+
+    const clearAndFillTestScriptArea = async (script) => {
+      try {
+        // click the view tests button to reveal the test code editor
+        await app.client.$('span=View Tests').click();
+        // set the value of the code editor to be some hard coded simple assertion tests
+        await composerObj.clearTestScriptAreaAndWriteKeys(script);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    const addAndSend = async () => {
+      try {
+        await composerObj.addRequestBtn.click();
+        await workspaceObj.latestSendRequestBtn.click();
+      } catch(err) {
+        console.error(err);
+      }
+    };
+
+    // ==================================================================
+
+    describe("simple assertions w/ chai.assert", function() {
+
+      before("CLEAR DB", (done) => {
+        chai.request("http://localhost:3000")
+          .get("/clear")
+          .end(function(err, res) {
+            done();                               // <= Call done to signal callback end
+          });
+      });
+
+      after("CLEAR DB", (done) => {
+        chai.request("http://localhost:3000")
+          .get("/clear")
+          .send()
+          .end(function(err, res) {
+            done();                               // <= Call done to signal callback end
+          });
+      });
+
+      it("a simple assertion should PASS when making a GET request", async function() {
+        try {
+          const url = "http://jsonplaceholder.typicode.com/posts";
+          const method = "GET";
+          const script = "assert.strictEqual(3, 3, 'correct types');";
+          await fillRestRequest(url, method);
+          await clearAndFillTestScriptArea(script);
+          await addAndSend();
+          await new Promise((resolve) => 
+            setTimeout(async () => {
+              await app.client.$('a=Tests').click();
+              const testStatus = await app.client.$('#TestResult-0-status').getText();
+              expect(testStatus).to.equal("PASS");
+              resolve();
+            }, 500)
+          );
+        } catch (err) {
+          console.error(err);
+        }
+      });
+    });
   });
 }
