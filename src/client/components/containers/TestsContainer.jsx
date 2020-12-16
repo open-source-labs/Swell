dea2b6cd5ceb1b338b3e26b73a6414241291753c
jasonligg <9956289+jasonligg@users.noreply.github.com>
2020-12-16 10:48:27 -0500

    TestContainer component only renders SingleTestContainer if there is TestConent on the response object

--- src/client/components/containers/TestsContainer.jsx
+++ src/client/components/containers/TestsContainer.jsx
@@ -3,7 +3,9 @@ import EmptyState from "../display/EmptyState";
 import SingleTestContainer from './SingleTestContainer';
 
 export default function TestsContainer({ currentResponse }) {
-
-  return currentResponse.response ? 
-  <SingleTestContainer currentResponse={currentResponse} /> : <EmptyState />;
+  return (
+    currentResponse.response && currentResponse.response.testContent ? 
+    <SingleTestContainer currentResponse={currentResponse} /> 
+    : <EmptyState />
+  );
 }
