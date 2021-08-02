466cd8c9154054144c103c81bf003ded7168816d
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-02 15:33:47 -0700

    rename webrtc body form to server

--- src/client/components/composer/NewRequest/WebRTCServerEntryForm.jsx
+++ src/client/components/composer/NewRequest/WebRTCServerEntryForm.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Controlled as CodeMirror } from "react-codemirror2";
-import "codemirror/addon/edit/matchbrackets";
-import "codemirror/addon/edit/closebrackets";
-import "codemirror/theme/twilight.css";
-import "codemirror/lib/codemirror.css";
-import "codemirror/addon/hint/show-hint";
-import "codemirror/addon/hint/show-hint.css";
-import "codemirror-graphql/hint";
-import "codemirror-graphql/lint";
-import "codemirror-graphql/mode";
-import "codemirror/addon/lint/lint.css";
+import React, { useState, useEffect } from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/theme/twilight.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror-graphql/hint';
+import 'codemirror-graphql/lint';
+import 'codemirror-graphql/mode';
+import 'codemirror/addon/lint/lint.css';
 
-const WebRTCBodyEntryForm = (props) => {
+const WebRTServerEntryForm = (props) => {
   const {
     newRequestBody,
     newRequestBody: { bodyContent },
@@ -34,15 +34,15 @@ const WebRTCBodyEntryForm = (props) => {
         // conditionally render warning message
         warningMessage ? <div>{warningMessage.body}</div> : null
       }
-      <div className="composer-section-title">Body</div>
+      <div className="composer-section-title">TURN or STUN Servers</div>
       <div id="gql-body-entry" className="is-neutral-200-box p-3">
         <CodeMirror
           value={cmValue}
           options={{
-            mode: "graphql",
-            theme: "neo sidebar",
-            scrollbarStyle: "native",
-            lineNumbers: false,
+            mode: 'javascript',
+            theme: 'neo sidebar',
+            scrollbarStyle: 'native',
+            lineNumbers: true,
             lint: true,
             hintOptions: true,
             matchBrackets: true,
@@ -51,7 +51,7 @@ const WebRTCBodyEntryForm = (props) => {
             tabSize: 2,
           }}
           editorDidMount={(editor) => {
-            editor.setSize("100%", 150);
+            editor.setSize('100%', 150);
           }}
           onBeforeChange={(editor, data, value) => {
             const optionObj = {
@@ -59,8 +59,8 @@ const WebRTCBodyEntryForm = (props) => {
               completeSingle: false,
             };
             setValue(value);
-            editor.setOption("lint", optionObj);
-            editor.setOption("hintOptions", optionObj);
+            editor.setOption('lint', optionObj);
+            editor.setOption('hintOptions', optionObj);
           }}
           onChange={(editor, data, value) => {
             editor.showHint();
@@ -76,4 +76,4 @@ const WebRTCBodyEntryForm = (props) => {
   );
 };
 
-export default WebRTCBodyEntryForm;
+export default WebRTServerEntryForm;
