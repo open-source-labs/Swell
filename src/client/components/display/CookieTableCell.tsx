d7a5346de7c76800a6c2b5c5647d335d59e5f043
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-04 14:11:33 -0700

    removed unsued components

--- src/client/components/display/CookieTableCell.tsx
+++
@@ -1,9 +0,0 @@
-// /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// import * as React from 'react';
-// import { CookieProps } from '../../../types';
-
-// const CookieTableCell = ({ detail }: CookieProps) => (
-//   <div className="cookieTableCell">{detail.toString()}</div>
-// );
-
-// export default CookieTableCell;
