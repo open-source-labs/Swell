e11e714dd03b9401d0287a2ded70fbb566f1c7c4
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-08 21:34:53 -0700

    clean up

--- src/client/components/display/WebRTCRequestContent.jsx
+++ src/client/components/display/WebRTCRequestContent.jsx
@@ -1,109 +1,69 @@
-import React from 'react';
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { use } from 'chai';
+import React, { useState, useEffect, useRef } from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
-import Peer from '../../controllers/webrtcPeerController'
+import Peer from '../../controllers/webrtcPeerController';
 
-
-// Clearly this should be wired into actual Redux store
-// if we start really wiring up webrtc functionality
-// localSdp = '';
-
-
-// //you can specify a STUN server here
-const iceConfiguration = {};
-iceConfiguration.iceServers = [
-  {
-    // new coturn STUN/TURN
-    urls: 'turn:104.153.154.109',
-    username: 'teamswell',
-    credential: 'cohortla44',
-    credentialType: 'password',
-  },
-  {
-    urls: 'stun:104.153.154.109',
-  },
-  {
-    urls: 'stun:stun1.l.google.com:19302',
-  },
-];
-
-
+const jBeautify = require('js-beautify').js;
 
 export default function WebRTCRequestContent({ content }) {
-  // Clearly this should be wired into actual Redux store
-  // if we start really wiring up webrtc functionality
+  console.log('content', content);
+  const { body } = content.request;
+  const { iceConfiguration } = content.request.body;
+  const [connection, setconnection] = useState(null);
+  const [channel, setChannel] = useState(null);
   const [localSdp, setLocalSdp] = React.useState('');
   const [remoteSdp, setRemoteSdp] = React.useState('');
-  
-  console.log(`content`, content);
-  console.log(`request`, content.request);
-  console.log(`webrtc data`, content.webrtcData);
-  const { body } = content.request;
+  const [pcInitiator, setPcInitiator] = useState(null);
 
-  console.log(remoteSdp);
+  useEffect(() => {
+    console.log(pcInitiator);
+    if (pcInitiator) {
+      setLocalSdp(pcInitiator.connection.localDescription.sdp);
+    }
+  }, [pcInitiator]);
 
-  function createLocalSDP(config){
-    // add pcInitiator to global properties and instantiate a new Peer
-    let configuration = {};
-    // ***** Having trouble with JSON object coversion from "body"
-    // ***** when added to configuration and passed into Peer,
-    // ***** we get ERROR: Failed to construct 'RTCPeerConnection': cannot convert to dictionary.
-    // ***** so skipping this for the commmit
-    //
-    // if (config) {
-    //   console.log('server info from UI:');
-    //   console.log(config);
-    //   configuration.iceServers = [...config];
-    //   console.log('configuration set via UI:');
-    //   console.log(configuration);
-    // } else {
-    //   configuration = iceConfiguration;
-    // }
-    configuration = iceConfiguration;
-    globalThis.pcInitiator = new Peer(configuration);
-    pcInitiator.role = pcInitiator._roles.INITIATOR;
-    pcInitiator.initDataChannelAndEvents();
-    console.log('[WebRTCRequestContent][createLocalSdp] pcInitiator.createLocalSdp()');
-    // createLocalSdp now returns the localDescription once the offer is gereated and set
-    // so assign that to localSDP
-    return pcInitiator.createLocalSdp();
-  }
-
-  function setRemoteSDP(data) {
-    // console.log(e);
-    console.log('setRemoteSDP', data);
-
-    // setRemoteSdp(data);
+  function createLocalSDP() {
+    const pc = new Peer(iceConfiguration);
+    pc.role = content.request.method;
+    pc.initDataChannelAndEvents();
+    pc.createLocalSdp();
+    setPcInitiator(pc);
   }
 
   return (
     <div>
       <div className="p-3">
-        <div>
-          <div className="is-size-7">Servers</div>
-          <CodeMirror
-            value={body}
-            options={{
-              mode: 'application/json',
-              theme: 'neo readonly',
-              lineNumbers: true,
-              tabSize: 4,
-              lineWrapping: true,
-              readOnly: true,
-            }}
-          />
-        </div>
+        <div className="is-size-7">Servers</div>
+        <CodeMirror
+          value={jBeautify(JSON.stringify(body.iceConfiguration.iceServers))}
+          options={{
+            mode: 'javascript',
+            theme: 'neo sidebar',
+            scrollbarStyle: 'native',
+            lineNumbers: true,
+            lint: true,
+            hintOptions: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            indentUnit: 1,
+            tabSize: 1,
+          }}
+          editorDidMount={(editor) => {
+            editor.setSize('100%', '100%');
+          }}
+        />
       </div>
       <div className="columns p-3">
         <div className="column is-flex is-half is-flex-direction-column">
           <div className="is-size-7">Local SDP</div>
           <div className="columns">
             <div
-              style={{ maxWidth: `100%` }}
+              style={{ maxWidth: `100%`, height: '100%' }}
               className="column is-flex is-flex-direction-column"
             >
               <CodeMirror
-                value={localSdp || 'No SDP yet'} // this is not updated when the value in local state changes.  Should prob be added to Redux store, anyway.
+                value={localSdp || 'No SDP yet'}
                 options={{
                   mode: 'application/json',
                   theme: 'neo readonly',
@@ -113,9 +73,9 @@ export default function WebRTCRequestContent({ content }) {
                   readOnly: true,
                 }}
               />
-              <button 
+              <button
                 className="button is-webrtc"
-                onClick={() => setLocalSdp( createLocalSDP( body))}
+                onClick={() => createLocalSDP()}
               >
                 Create Local SDP
               </button>
@@ -128,7 +88,7 @@ export default function WebRTCRequestContent({ content }) {
             <div className="column is-flex is-flex-direction-column">
               <CodeMirror
                 value={remoteSdp || 'No SDP yet'}
-                onChange={setRemoteSDP}
+                // onChange={setRemoteSDP}
                 options={{
                   mode: 'application/json',
                   theme: 'neo readonly',
