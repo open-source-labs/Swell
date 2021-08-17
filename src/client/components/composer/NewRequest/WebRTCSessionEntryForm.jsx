2b993fd6ab702b45dbf54bd844adcb1496e00569
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2021-08-16 17:14:53 -0700

    clean up webrtc url entry form

--- src/client/components/composer/NewRequest/WebRTCSessionEntryForm.jsx
+++ src/client/components/composer/NewRequest/WebRTCSessionEntryForm.jsx
@@ -10,113 +10,17 @@ const WebRTCSessionEntryForm = ({
   setNewRequestFields,
   newRequestFields,
 }) => {
-  const [dropdownIsActive, setDropdownIsActive] = useState(false);
-  const dropdownEl = useRef();
-
-  useEffect(() => {
-    const closeDropdown = (event) => {
-      if (!dropdownEl.current.contains(event.target)) {
-        setDropdownIsActive(false);
-      }
-    };
-    document.addEventListener('click', closeDropdown);
-    return () => document.removeEventListener('click', closeDropdown);
-  }, []);
-
-  const warningCheck = () => {
-    if (warningMessage.uri) {
-      const newWarningMessage = { ...warningMessage };
-      delete warningMessage.uri;
-      setComposerWarningMessage({ ...newWarningMessage });
-    }
-  };
-
-  const urlChangeHandler = (e) => {
-    warningCheck();
-    const url = e.target.value;
-    setNewRequestFields({
-      ...newRequestFields,
-      webrtcUrl: url,
-      url,
-    });
-  };
-
-  const methodChangeHandler = (value) => {
-    warningCheck();
-    let newBody;
-    setNewRequestFields({
-      ...newRequestFields,
-      method: value,
-      bodyContent: newBody,
-      bodyIsNew: false,
-    });
-  };
-
   return (
-    <div>
-      <div
-        ref={dropdownEl}
-        className={`ml-2 mr-2 is-flex is-justify-content-center dropdown ${
-          dropdownIsActive ? 'is-active' : ''
-        }`}
-      >
-        <div className="dropdown-trigger">
-          <button
-            className="button is-webrtc"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu"
-            onClick={() => setDropdownIsActive(!dropdownIsActive)}
-          >
-            <span>{newRequestFields.method}</span>
-            <span className="icon is-small">
-              <img
-                src={dropDownArrow}
-                className="is-awesome-icon"
-                aria-hidden="true"
-                alt="dropdown"
-              />
-            </span>
-          </button>
-        </div>
-
-        <div className="dropdown-menu" id="dropdown-menu">
-          <ul className="dropdown-content">
-            {newRequestFields.method !== 'INITIATOR' && (
-              <a
-                onClick={() => {
-                  setDropdownIsActive(false);
-                  methodChangeHandler('INITIATOR');
-                }}
-                className="dropdown-item"
-              >
-                INITIATOR
-              </a>
-            )}
-            {newRequestFields.method !== 'RECEIVER' && (
-              <a
-                onClick={() => {
-                  setDropdownIsActive(false);
-                  methodChangeHandler('RECEIVER');
-                }}
-                className="dropdown-item"
-              >
-                RECEIVER
-              </a>
-            )}
-          </ul>
-        </div>
-
-        <input
-          className="ml-1 input input-is-medium is-info"
-          type="text"
-          placeholder="Enter endpoint"
-          value={newRequestFields.url}
-          onChange={(e) => {
-            urlChangeHandler(e);
-          }}
-        />
+    <div className="is-flex is-justify-content-center">
+      <div id="webRTButton" className="button is-webrtc">
+        <span>SDP</span>
       </div>
-
+      <input
+        className="ml-1 input input-is-medium is-info"
+        type="text"
+        placeholder="No url needed"
+        disabled
+      />
       {warningMessage.uri && (
         <div className="warningMessage">{warningMessage.uri}</div>
       )}
