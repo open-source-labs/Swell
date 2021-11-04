a3edcb6726505a1a74ec137017df34bfa9d43f88
Christopher <christopherjpan@gmail.com>
2021-11-04 03:00:30 -0700

    broken package.json

--- webpack.development.js
+++ webpack.development.js
@@ -20,11 +20,12 @@ module.exports = merge(base, {
   //   //   secure: false,
   //   // },
   // },
+  // client: {
+    // webSocketTransport: require.resolve('./src/server/server.js'),
+  // },
   devServer: {
     host: 'localhost',
-    // client: {
-    //   webSocketTransport: require.resolve('./src/server/server.js'),
-    // },
+
     port: '8080',
     hot: true,
     // static: {
@@ -59,3 +60,29 @@ module.exports = merge(base, {
     },
   },
 });
+
+
+// module.exports = merge(base, {
+//   mode: 'development',
+//   devtool: 'source-map',
+//   devServer: {
+//     host: 'localhost',
+//     port: '8080',
+//     hot: true,
+//     compress: true,
+//     contentBase: path.resolve(__dirname, 'dist'),
+//     watchContentBase: true,
+//     watchOptions: {
+//       ignored: /node_modules/,
+//     },
+//     before() {
+//       spawn('electron', ['.', 'dev'], {
+//         shell: true,
+//         env: process.env,
+//         stdio: 'inherit',
+//       })
+//         .on('close', (code) => process.exit(0))
+//         .on('error', (spawnError) => console.error(spawnError));
+//     },
+//   },
+// });
