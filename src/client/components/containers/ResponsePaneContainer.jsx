d67500435dc225dc58fdf6a51cce5165403d8112
Christopher <christopherjpan@gmail.com>
2021-11-11 10:41:18 -0800

    added button back in responsePaneContainer

--- src/client/components/containers/ResponsePaneContainer.jsx
+++ src/client/components/containers/ResponsePaneContainer.jsx
@@ -30,15 +30,15 @@ const ResponsePaneContainer = () => {
 
 
   //VVVV get rid of this button
-  // const testing = () => {
-  //   console.log('testing', currentResponse);
-  // }
+  const testing = () => {
+    console.log('testing', currentResponse);
+  }
   return (
     <div
       className="column is-one-third is-flex is-flex-direction-column is-tall"
       id="responses"
     >
-      {/* <button onClick={() => testing()}>></button> */}
+      <button onClick={() => testing()}>></button>
       {/* HEADER */}
       <div className="hero is-primary header-bar is-flex is-flex-direction-row is-justify-content-center">
         <ResponseTime currentResponse={currentResponse} />
