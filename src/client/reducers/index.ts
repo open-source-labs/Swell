f145ebc4b9493ebec23a53513336cd9e46386107
kurto8 <90794630+kurto8@users.noreply.github.com>
2021-12-29 16:05:23 -0700

    business.TS Co-authored-by: kurto8 <kurto8@users.noreply.github.com> Co-authored-by: gigifeeds <gigifeeds@users.noreply.github.com> Co-authored-by: hanswand <hanswand@users.noreply.github.com> Co-authored-by: samhpyo <samhpyo@users.noreply.github.com> Co-authored-by: thedrewery <thedrewery@users.noreply.github.com>

--- src/client/reducers/index.ts
+++ src/client/reducers/index.ts
@@ -7,6 +7,7 @@ import uiReducer from './ui';
 // combine reducers
 const reducers = combineReducers({
   business: businessReducer,
+  // @ts-expect-error ts-migrate(2322) FIXME: Type '(state: { composerDisplay: string; sidebarAc... Remove this comment to see the full error message
   ui: uiReducer,
 });
 
