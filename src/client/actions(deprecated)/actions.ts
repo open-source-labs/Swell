b14813591b8dc4bf3c5bca57accee0d1f63be55f
Parkreiner <throwawayclover@gmail.com>
2022-06-21 15:10:30 -0400

    chore: Run prettier on file

--- src/client/actions(deprecated)/actions.ts
+++ src/client/actions(deprecated)/actions.ts
@@ -7,209 +7,276 @@ import * as types from './actionTypes';
 
 // action creators
 // SPNOTE START line 11 types.ts history: Record<string, unknown>[];
-export const getHistory = (history: { date: string; history: ReqRes[]; }[]): {type: string, payload: { date: string; history: ReqRes[]; }[]} => ({
+export const getHistory = (
+  history: { date: string; history: ReqRes[] }[]
+): { type: string; payload: { date: string; history: ReqRes[] }[] } => ({
   type: types.GET_HISTORY,
-  payload: history
+  payload: history,
 });
 
-export const deleteFromHistory = (reqRes: string): {type: string, payload: string} => ({
+export const deleteFromHistory = (
+  reqRes: string
+): { type: string; payload: string } => ({
   type: types.DELETE_HISTORY,
-  payload: reqRes
+  payload: reqRes,
 });
 
-export const clearHistory = (): {type: string} => ({
+export const clearHistory = (): { type: string } => ({
   type: types.CLEAR_HISTORY,
 });
 
 // SPNOTE line 11 types.ts collections: Record<string, unknown>[];
-export const getCollections = (collections: string[]): {type: string, payload: string[]} => ({
+export const getCollections = (
+  collections: string[]
+): { type: string; payload: string[] } => ({
   type: types.GET_COLLECTIONS,
-  payload: collections
+  payload: collections,
 });
 
-export const deleteFromCollection = (collection: string): {type: string, payload: string} => ({
+export const deleteFromCollection = (
+  collection: string
+): { type: string; payload: string } => ({
   type: types.DELETE_COLLECTION,
-  payload: collection
+  payload: collection,
 });
 
 // SPNOTE line 9 types.ts reqResArray: NewRequestResponseObject[];
-export const collectionToReqRes = (reqResArray: ReqRes[]): {type: string, payload: ReqRes[]}  => ({
+export const collectionToReqRes = (
+  reqResArray: ReqRes[]
+): { type: string; payload: ReqRes[] } => ({
   type: types.COLLECTION_TO_REQRES,
-  payload: reqResArray
+  payload: reqResArray,
 });
 
-export const collectionAdd = (collection: string): {type: string, payload: string} => ({
+export const collectionAdd = (
+  collection: string
+): { type: string; payload: string } => ({
   type: types.COLLECTION_ADD,
-  payload: collection
+  payload: collection,
 });
 
-export const collectionUpdate = (collection: string): {type: string, payload: string} => ({
+export const collectionUpdate = (
+  collection: string
+): { type: string; payload: string } => ({
   type: types.COLLECTION_UPDATE,
-  payload: collection
+  payload: collection,
 });
 
-export const reqResClear = (): {type: string} => ({
+export const reqResClear = (): { type: string } => ({
   type: types.REQRES_CLEAR,
 });
 
-export const reqResAdd = (reqRes: ReqRes): {type: string, payload: ReqRes} => ({
+export const reqResAdd = (
+  reqRes: ReqRes
+): { type: string; payload: ReqRes } => ({
   type: types.REQRES_ADD,
-  payload: reqRes
+  payload: reqRes,
 });
 
-export const reqResDelete = (reqRes: ReqRes): {type: string, payload: ReqRes} => ({
+export const reqResDelete = (
+  reqRes: ReqRes
+): { type: string; payload: ReqRes } => ({
   type: types.REQRES_DELETE,
-  payload: reqRes
+  payload: reqRes,
 });
 
-export const reqResUpdate = (reqRes: ReqRes): {type: string, payload: ReqRes} => ({
+export const reqResUpdate = (
+  reqRes: ReqRes
+): { type: string; payload: ReqRes } => ({
   type: types.REQRES_UPDATE,
-  payload: reqRes
+  payload: reqRes,
 });
 
 //reqRes payload type initially assigned to a boolean
-export const scheduledReqResUpdate = (reqRes: ReqRes): {type: string, payload: ReqRes} => ({
+export const scheduledReqResUpdate = (
+  reqRes: ReqRes
+): { type: string; payload: ReqRes } => ({
   type: types.SCHEDULED_REQRES_UPDATE,
-  payload: reqRes
+  payload: reqRes,
 });
 
-export const scheduledReqResDelete = (): {type: string} => ({
+export const scheduledReqResDelete = (): { type: string } => ({
   type: types.SCHEDULED_REQRES_DELETE,
 });
 
-export const updateGraph = (id: number): {type: string, payload: number} => ({
+export const updateGraph = (id: number): { type: string; payload: number } => ({
   type: types.UPDATE_GRAPH,
-  payload: id
+  payload: id,
 });
 
-export const clearGraph = (reqRes: Record<string, number>): {type: string, payload: Record<string, number>} => ({
+export const clearGraph = (
+  reqRes: Record<string, number>
+): { type: string; payload: Record<string, number> } => ({
   type: types.CLEAR_GRAPH,
-  payload: reqRes
+  payload: reqRes,
 });
 
-export const clearAllGraph = (): {type: string} => ({
+export const clearAllGraph = (): { type: string } => ({
   type: types.CLEAR_ALL_GRAPH,
 });
 
-export const setComposerWarningMessage = (message: Record<string, string>): {type: string, payload: Record<string, string>} => ({
+export const setComposerWarningMessage = (
+  message: Record<string, string>
+): { type: string; payload: Record<string, string> } => ({
   type: types.SET_COMPOSER_WARNING_MESSAGE,
-  payload: message
+  payload: message,
 });
 
-export const resetComposerFields = (): {type: string} => ({
+export const resetComposerFields = (): { type: string } => ({
   type: types.RESET_COMPOSER_FIELDS,
 });
 
-export const setNewRequestFields = (requestObj: Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+export const setNewRequestFields = (
+  requestObj: Record<string, unknown>
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_NEW_REQUEST_FIELDS,
-  payload: requestObj
+  payload: requestObj,
 });
 
 // GIGI START HERE
 // gigi - because of line 164 in History.jsx
-export const setNewRequestHeaders = (headers: string[]): {type: string, payload: string[]} => ({
+export const setNewRequestHeaders = (
+  headers: string[]
+): { type: string; payload: string[] } => ({
   type: types.SET_NEW_REQUEST_HEADERS,
-  payload: headers
+  payload: headers,
 });
 
 // gigi - SingleReqResContainer line 172 & History.jsx 189
-export const setNewRequestStreams = (streams: Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+export const setNewRequestStreams = (
+  streams: Record<string, unknown>
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_NEW_REQUEST_STREAMS,
-  payload: streams
+  payload: streams,
 });
 
 // gigi - history line 171 & singlereqrescontainer line 149
-export const setNewRequestBody = (body: string): {type: string, payload: string} => ({
+export const setNewRequestBody = (
+  body: string
+): { type: string; payload: string } => ({
   type: types.SET_NEW_REQUEST_BODY,
-  payload: body
+  payload: body,
 });
 
 // gigi - because of line 84 in types.ts //testContent in bussiness.ts
-export const setNewTestContent = (content: string): {type: string, payload: string} => ({
+export const setNewTestContent = (
+  content: string
+): { type: string; payload: string } => ({
   type: types.SET_NEW_TEST_CONTENT,
-  payload: content
+  payload: content,
 });
 
-
-export const setNewRequestCookies = (cookies: string[]): {type: string, payload: string[]} => ({
+export const setNewRequestCookies = (
+  cookies: string[]
+): { type: string; payload: string[] } => ({
   type: types.SET_NEW_REQUEST_COOKIES,
-  payload: cookies // jNote - no breakage, but cookies don't seem to work correctly
+  payload: cookies, // jNote - no breakage, but cookies don't seem to work correctly
 });
 
-export const setNewRequestSSE = (SSEBool: boolean): {type: string, payload: boolean} => ({
+export const setNewRequestSSE = (
+  SSEBool: boolean
+): { type: string; payload: boolean } => ({
   type: types.SET_NEW_REQUEST_SSE,
-  payload: SSEBool
+  payload: SSEBool,
 });
 
-export const setChecksAndMinis = (reqResArray: string[]): {type: string, payload: string[]} => ({
+export const setChecksAndMinis = (
+  reqResArray: string[]
+): { type: string; payload: string[] } => ({
   type: types.SET_CHECKS_AND_MINIS,
-  payload: reqResArray
+  payload: reqResArray,
 }); //
 
-export const setIntrospectionData = (dataObj: Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+export const setIntrospectionData = (
+  dataObj: Record<string, unknown>
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_INTROSPECTION_DATA,
-  payload: dataObj
+  payload: dataObj,
 });
 
 // There is a second parameter (of varying type: string or object) that is sometimes passed to this function
-export const saveCurrentResponseData = (dataObj: Record<string, unknown>, secondParam: string | Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+export const saveCurrentResponseData = (
+  dataObj: Record<string, unknown>,
+  secondParam: string | Record<string, unknown>
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SAVE_CURRENT_RESPONSE_DATA,
   payload: dataObj,
 });
 
-export const setSidebarActiveTab = (tabName: string): {type: string, payload: string} => ({
+export const setSidebarActiveTab = (
+  tabName: string
+): { type: string; payload: string } => ({
   type: types.SET_SIDEBAR_ACTIVE_TAB,
-  payload: tabName
+  payload: tabName,
 });
 
-
-export const setWorkspaceActiveTab = (tabName: string): {type: string, payload: string} => ({
+export const setWorkspaceActiveTab = (
+  tabName: string
+): { type: string; payload: string } => ({
   type: types.SET_WORKSPACE_ACTIVE_TAB,
-  payload: tabName
+  payload: tabName,
 });
 
-
-export const setResponsePaneActiveTab = (tabName: string): {type: string, payload: string} => ({
+export const setResponsePaneActiveTab = (
+  tabName: string
+): { type: string; payload: string } => ({
   type: types.SET_RESPONSE_PANE_ACTIVE_TAB,
-  payload: tabName
+  payload: tabName,
 });
 
-export const toggleDarkMode = (isDark: boolean): {type: string, payload: boolean} => ({
+export const toggleDarkMode = (
+  isDark: boolean
+): { type: string; payload: boolean } => ({
   type: types.TOGGLE_DARK_MODE,
-  payload: isDark
-})
+  payload: isDark,
+});
 
 export const setNewRequestsOpenAPI = ({
   openapiMetadata,
-  openapiReqArray
-}: Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+  openapiReqArray,
+}: Record<string, unknown>): {
+  type: string;
+  payload: Record<string, unknown>;
+} => ({
   type: types.SET_NEW_REQUESTS_OPENAPI,
   payload: { openapiMetadata, openapiReqArray },
 });
 
-
-export const setOpenAPIServersGlobal = (serverIds: []): {type: string, payload: []} => ({
+export const setOpenAPIServersGlobal = (
+  serverIds: []
+): { type: string; payload: [] } => ({
   type: types.SET_OPENAPI_SERVERS_GLOBAL,
-  payload: serverIds
+  payload: serverIds,
 });
 
-
-export const setOpenAPIServers = (requestId: number | string, serverIds: []): {type: string, payload: Record<string, unknown>} => ({
+export const setOpenAPIServers = (
+  requestId: number | string,
+  serverIds: []
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_NEW_OPENAPI_SERVERS,
-  payload: { id: requestId, serverIds }
+  payload: { id: requestId, serverIds },
 });
 
-
-export const setOpenAPIParameter = (requestId: number | string, location: string, name: string, value: number | string): {type: string, payload: Record<string, unknown>} => ({
+export const setOpenAPIParameter = (
+  requestId: number | string,
+  location: string,
+  name: string,
+  value: number | string
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_NEW_OPENAPI_PARAMETER,
-  payload: { id: requestId, location, name, value }
+  payload: { id: requestId, location, name, value },
 });
 
-export const setOpenAPIRequestBody = (requestId: number | string, mediaType: string, requestBody: Record<string, unknown>): {type: string, payload: Record<string, unknown>} => ({
+export const setOpenAPIRequestBody = (
+  requestId: number | string,
+  mediaType: string,
+  requestBody: Record<string, unknown>
+): { type: string; payload: Record<string, unknown> } => ({
   type: types.SET_NEW_OPENAPI_REQUEST_BODY,
-  payload: { id: requestId, mediaType, requestBody }
+  payload: { id: requestId, mediaType, requestBody },
 });
 
-export const queueOpenAPIRequests = (): {type: string} => ({
+export const queueOpenAPIRequests = (): { type: string } => ({
   type: types.QUEUE_OPENAPI_REQUESTS,
 });
+
