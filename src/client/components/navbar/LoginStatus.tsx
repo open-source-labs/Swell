fea77de766d5278f4d53fd2368c07d6849c39af4
Jack <jackksono@gmail.com>
2023-02-17 10:37:19 -0800

    Updated styling to NavBar Components; highlights selected composer Co-authored-by: Mihran <mihran-baytaryan@users.noreply.github.com>

--- src/client/components/navbar/LoginStatus.tsx
+++ src/client/components/navbar/LoginStatus.tsx
@@ -109,7 +109,6 @@ export default function LoginContainer(props) {
       currentTeam: null,
       isActiveSession: false,
     });
-    console.log(endSessionLog);
   }
 
   return(
@@ -131,8 +130,7 @@ export default function LoginContainer(props) {
             <Button
               color='secondary'
               sx={{ maxWidth: '24px', maxHeight: '24px', minWidth: '24px', minHeight: '24px' }}
-              onClick={handleSignOut}
-            >
+              onClick={handleSignOut}>
               <LogoutRoundedIcon fontSize='small' />
             </Button>
           </LightTooltip>
