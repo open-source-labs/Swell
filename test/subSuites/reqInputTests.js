2c7f455ff08add38751e6d9e0ca12bef9ed763fe
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-03-18 09:53:33 -0700

    Revive `reqInputTests`
    Most existing tests work but some small changes
    in the element query selector are required to ensure
    the correct element is selected

--- test/subSuites/reqInputTests.js
+++ test/subSuites/reqInputTests.js
@@ -1,203 +1,200 @@
 // Testing UI that all protocol and request types are represented, parameters can be entered,
-// and panels appear appropriately in the application 
+// and panels appear appropriately in the application
 
-const {_electron: electron} = require('playwright');
-const chai = require('chai')
-const expect = chai.expect
+const { _electron: electron } = require('playwright');
+const pwTest = require('@playwright/test');
+const chai = require('chai');
+const expect = chai.expect;
 const path = require('path');
 const fs = require('fs-extra');
 
 let electronApp, page;
 
 module.exports = () => {
-
-  const setupFxn = function() {
+  describe('App opens and renders a page', () => {
     before(async () => {
       electronApp = await electron.launch({ args: ['main.js'] });
-      page = electronApp.windows()[0]; // In case there is more than one window
-      await page.waitForLoadState(`domcontentloaded`);
     });
-    
+
     // close Electron app when complete
     after(async () => {
       await electronApp.close();
     });
 
-    afterEach(async function() {
+    // If the test fails, take a screenshot of the app and save it into the "failedTests" directory under the test title
+    afterEach(async function () {
       if (this.currentTest.state === 'failed') {
-        console.log(`Screenshotting failed test window`)
-        const imageBuffer = await page.screenshot();
-        fs.writeFileSync(path.resolve(__dirname + '/../failedTests', `FAILED_${this.currentTest.title}.png`), imageBuffer)
+        console.log(`Screenshotting failed test window`);
+        const window = await electronApp.firstWindow();
+        const imageBuffer = await window.screenshot();
+        fs.writeFileSync(
+          path.resolve(
+            __dirname + '/../failedTests',
+            `FAILED_${this.currentTest.title}.png`
+          ),
+          imageBuffer
+        );
       }
     });
-  }
 
-
-
-  describe('URL/request method inputs', () => {
-    setupFxn();
-
-    it('can view history in the composer pane', async () => { // May refactor since history tab will be a dropdown1
-      
-      // Click history, confirm history column is active
-      await page.locator('a >> text=History').click();
-      const historySelected = await page.locator('div[id^="composer"] >> .is-active').innerText();
-      expect(historySelected).to.equal('History');
-
-      // Click composer, confirm composer column is active
-      await page.locator('a >> text=Composer').click();
-      const composerSelected = await page.locator('div#composer >> .is-active').innerText();
-      expect(composerSelected).to.equal('Composer');
-    });
-
-    it('can switch tabs in the workspace pane', async () => {
-      
-      // Click saved workspace, confirm column is active
-      await page.locator('a >> text=Saved Workspace').click();
-      const savedSelected = await page.locator('div#workspace >> .is-active').innerText();
-      expect(savedSelected).to.equal('Saved Workspace');
-
-      // Click schedule, confirm column is active
-      await page.locator('a >> text=Schedule').click();
-      const scheduleSelected = await page.locator('div#workspace >> .is-active').innerText();
-      expect(scheduleSelected).to.equal('Schedule');
-
-      // Click requests, confirm column is active
-      await page.locator('a >> text=Requests').click();
-      const requestsSelected = await page.locator('div#workspace >> .is-active').innerText();
-      expect(requestsSelected).to.equal('Requests');
-      
-    });
-
-    it('can switch tabs in the responses pane', async () => {
-      
-      // Click headers, confirm column is active
-      await page.locator('a >> text=Headers').click();
-      const headersSelected = await page.locator('div#responses >> .is-active').innerText();
-      expect(headersSelected).to.equal('Headers');
-
-      // Click cookies, confirm column is active
-      await page.locator('a >> text=Cookies').click();
-      const cookiesSelected = await page.locator('div#responses >> .is-active').innerText();
-      expect(cookiesSelected).to.equal('Cookies');
-
-      // Click tests, confirm column is active
-      await page.locator('a >> text=Tests').click();
-      const testsSelected = await page.locator('div#responses >> .is-active').innerText();
-      expect(testsSelected).to.equal('Tests');
-
-      // Click events, confirm column is active
-      await page.locator('a >> text=Events').click();
-      const eventsSelected = await page.locator('div#responses >> .is-active').innerText();
-      expect(eventsSelected).to.equal('Events');
-      
-    });
-
-
-    it('can select a request type', async () => {
-
-      await page.locator('button>> text=GRAPHQL').click();
-      expect(await page.locator('div#composer-graphql').count()).to.equal(1)
-
-      await page.locator('button>> text=HTTP2').click();
-      expect(await page.locator('div#composer-http2').count()).to.equal(1)
-
-      await page.locator('button>> text=GRPC').click();
-      expect(await page.locator('div#composer-grpc').count()).to.equal(1)
-
-      await page.locator('button>> text=WEB SOCKET').click();
-      expect(await page.locator('div#composer-websocket').count()).to.equal(1)
-
-      await page.locator('button>> text=WEBRTC').click();
-      expect(await page.locator('div#composer-webrtc').count()).to.equal(1)
-
-      await page.locator('button>> text=OPENAPI').click();
-      expect(await page.locator('div#composer-openapi').count()).to.equal(1)
-
-      await page.locator('button>> text=WEBHOOK').click();
-      expect(await page.locator('div#composer-webhook').count()).to.equal(1)
-    });
-
-    // WIP code below to check dropdown menu items
-      // const getButton = await page.locator('span >> text=GET')
-      // expect(getButton).to.exist;
-
-      // await getButton.click()
-      // const methodMenuArray = await page.locator('div.dropdown.is-active >> .dropdown-item').allInnerTexts();
-      // expect(methodMenuArray).to.include('POST').and.to.include('PUT').and.to.include('PATCH').and.to.include('DELETE')
-          // const requestDropdown = await page.locator('.dropdown .is-active');
-      // console.log(await requestDropdown.locator('.dropdown-content'))
-
-    it('can select a REST method', async () => { // ************************ REDO THIS TEST, it chains clicks in a bad way ********************
-
-      // Make sure HTTP2 method is selected
-      await page.locator('button>> text=HTTP2').click();
-
-
-      // // click and select POST
-      await page.locator('button#rest-method').click();
-      await page.locator('div[id^="composer"] >> a >> text=POST').click();
-      expect(await page.locator('button.is-rest >> span >> text=POST').count()).to.equal(1);
-
-
-      // // click and select PUT
-      await page.locator('button#rest-method').click();
-      await page.locator('div[id^="composer"] >> a >> text=PUT').click();
-      expect(await page.locator('button.is-rest >> span >> text=PUT').count()).to.equal(1);
-
-      // // click and select GET
-      await page.locator('button#rest-method').click();
-      await page.locator('div[id^="composer"] >> a >> text=GET').click();
-      expect(await page.locator('button.is-rest >> span >> text=GET').count()).to.equal(1);
-
-      // // click and select PATCH
-      await page.locator('button#rest-method').click();
-      await page.locator('div[id^="composer"] >> a >> text=PATCH').click();
-      expect(await page.locator('button.is-rest >> span >> text=PATCH').count()).to.equal(1);
-
-      // // click and select DELETE
-      await page.locator('button#rest-method').click();
-      await page.locator('div[id^="composer"] >> a >> text=DELETE').click();
-      expect(await page.locator('button.is-rest >> span >> text=DELETE').count()).to.equal(1);
-    });
-
-
-    it('can type url into url input', async () => {
-      await page.locator('#url-input').fill('http://jsonplaceholder.typicode.com/posts/1');
-
-      const input = await page.locator('#url-input').inputValue();
-
-      expect(input).to.equal('http://jsonplaceholder.typicode.com/posts/1')
-    });
-  }).timeout(20000);
-
-  
-  describe('REST parameter inputs', async () => { // Add tests for deleting header and checking/unchecking boxes
-    setupFxn();
-    describe('Header inputs', async () => {
-      it('should open headers input, rendering single input at first', async () => {
-        // count header rows
-        const count = await page.locator('.header-row').count();
-        expect(count).to.equal(1);
+    // The app takes a while to launch, and without these rendering checks
+    // within each test file the tests can get flakey because of long load times
+    // so these are here to ensure the app launches as expect before continuing
+    describe('Window rendering', () => {
+      it('Electron app should launch', async () => {
+        expect(electronApp).to.be.ok;
       });
 
-      it('can add new headers in request', async () => {
-        const addHeaderButton = await page.locator('button#add-header');
-        expect(await addHeaderButton.count()).to.equal(1);
-
-        await addHeaderButton.click();
-        expect(await page.locator('.header-row').count()).to.equal(2);
+      it('Electron app should be a visible window', async () => {
+        const window = await electronApp.firstWindow();
+        pwTest.expect(window).toBeVisible();
       });
-      
-      it('can type in keys & values to headers', async () => {
-        await page.locator('#header-row0 >> [placeholder="Key"]').fill('Checking header Key input!');
-        await page.locator('#header-row0 >> [placeholder="Value"]').fill('Checking header Value input!');
 
-        const headerKey = await page.locator('#header-row0 >> [placeholder="Key"]').inputValue();
-        const headerValue = await page.locator('#header-row0 >> [placeholder="Value"]').inputValue();
+      it('App should only have 1 window (i.e. confirm devTools is not open)', async () => {
+        expect(electronApp.windows().length).to.equal(1);
+      });
+    });
 
-        expect(headerKey).to.equal('Checking header Key input!');
-        expect(headerValue).to.equal('Checking header Value input!');
+    describe('URL/request method selection', () => {
+      before(async () => {
+        page = electronApp.windows()[0]; // In case there is more than one window
+        await page.waitForLoadState(`domcontentloaded`);
+      });
+
+      it('select GraphQL', async () => {
+        await page.locator('button>> text=GraphQL').click();
+        expect(await page.locator('div#composer-graphql').count()).to.equal(1);
+      });
+
+      it('select HTTP/2', async () => {
+        await page.locator('button>> text=HTTP/2').click();
+        expect(await page.locator('div#composer-http2').count()).to.equal(1);
+      });
+
+      it('select gRPC', async () => {
+        await page.locator('button>> text=gRPC').click();
+        expect(await page.locator('div#composer-grpc').count()).to.equal(1);
+      });
+
+      it('select WebSocket', async () => {
+        await page.locator('button>> text=WebSocket').click();
+        expect(await page.locator('div#composer-websocket').count()).to.equal(
+          1
+        );
+      });
+
+      it('select WebRTC', async () => {
+        await page.locator('button>> text=WebRTC').click();
+        expect(await page.locator('div#composer-webrtc').count()).to.equal(1);
+      });
+
+      it('select OpenAPI', async () => {
+        await page.locator('button>> text=OpenAPI').click();
+        expect(await page.locator('div#composer-openapi').count()).to.equal(1);
+      });
+
+      it('select Webhook', async () => {
+        await page.locator('button>> text=Webhook').click();
+        expect(await page.locator('div#composer-webhook').count()).to.equal(1);
+      });
+    });
+
+    describe('URL/request method inputs', () => {
+      before(async () => {
+        await page.locator('button>> text=HTTP/2').click();
+      });
+
+      beforeEach(async () => {
+        await page.locator('button#rest-method').click();
+      });
+
+      it('can select a POST request', async () => {
+        await page.locator('div[id^="composer"] >> a >> text=POST').click();
+        expect(
+          await page.locator('button.is-rest >> span >> text=POST').count()
+        ).to.equal(1);
+      });
+
+      it('can select a PUT request', async () => {
+        await page.locator('div[id^="composer"] >> a >> text=PUT').click();
+        expect(
+          await page.locator('button.is-rest >> span >> text=PUT').count()
+        ).to.equal(1);
+      });
+
+      it('can select a GET request', async () => {
+        await page.locator('div[id^="composer"] >> a >> text=GET').click();
+        expect(
+          await page.locator('button.is-rest >> span >> text=GET').count()
+        ).to.equal(1);
+      });
+
+      it('can select a PATCH request', async () => {
+        await page.locator('div[id^="composer"] >> a >> text=PATCH').click();
+        expect(
+          await page.locator('button.is-rest >> span >> text=PATCH').count()
+        ).to.equal(1);
+      });
+
+      it('can select a DELETE request', async () => {
+        await page.locator('div[id^="composer"] >> a >> text=DELETE').click();
+        expect(
+          await page.locator('button.is-rest >> span >> text=DELETE').count()
+        ).to.equal(1);
+      });
+    });
+
+    describe('HTTP/2 URL input successful', () => {
+      before(async () => {
+        await page.locator('button>> text=HTTP/2').click();
+      });
+
+      it('can type url into url input', async () => {
+        await page
+          .locator('#url-input')
+          .fill('http://jsonplaceholder.typicode.com/posts/1');
+
+        const input = await page.locator('#url-input').inputValue();
+
+        expect(input).to.equal('http://jsonplaceholder.typicode.com/posts/1');
+      });
+    });
+
+    describe('REST parameter inputs', async () => {
+      // Add tests for deleting header and checking/unchecking boxes
+      describe('Header inputs', async () => {
+        it('should open headers input, rendering single input at first', async () => {
+          // count header rows
+          const count = await page.locator('.header-row').count();
+          expect(count).to.equal(1);
+        });
+
+        it('can add new headers in request', async () => {
+          const addHeaderButton = await page.locator('button#add-header');
+          expect(await addHeaderButton.count()).to.equal(1);
+
+          await addHeaderButton.click();
+          expect(await page.locator('.header-row').count()).to.equal(2);
+        });
+
+        it('can type in keys & values to headers', async () => {
+          await page
+            .locator('#header-row0 >> [placeholder="Key"]')
+            .fill('Checking header Key input!');
+          await page
+            .locator('#header-row0 >> [placeholder="Value"]')
+            .fill('Checking header Value input!');
+
+          const headerKey = await page
+            .locator('#header-row0 >> [placeholder="Key"]')
+            .inputValue();
+          const headerValue = await page
+            .locator('#header-row0 >> [placeholder="Value"]')
+            .inputValue();
+
+          expect(headerKey).to.equal('Checking header Key input!');
+          expect(headerValue).to.equal('Checking header Value input!');
+        });
       });
     });
 
@@ -207,7 +204,7 @@ module.exports = () => {
         const count = await page.locator('.cookie-row').count();
         expect(count).to.equal(1);
       });
-  
+
       it('can add new cookies in request', async () => {
         // click add cookie
         const addCookieButton = await page.locator('button#add-cookie');
@@ -218,11 +215,19 @@ module.exports = () => {
       });
 
       it('can type in keys & values to cookies', async () => {
-        await page.locator('#cookie-row0 >> [placeholder="Key"]').fill('Checking cookie Key input!');
-        await page.locator('#cookie-row0 >> [placeholder="Value"]').fill('Checking cookie Value input!');
+        await page
+          .locator('#cookie-row0 >> [placeholder="Key"]')
+          .fill('Checking cookie Key input!');
+        await page
+          .locator('#cookie-row0 >> [placeholder="Value"]')
+          .fill('Checking cookie Value input!');
 
-        const cookieKey = await page.locator('#cookie-row0 >> [placeholder="Key"]').inputValue();
-        const cookieValue = await page.locator('#cookie-row0 >> [placeholder="Value"]').inputValue();
+        const cookieKey = await page
+          .locator('#cookie-row0 >> [placeholder="Key"]')
+          .inputValue();
+        const cookieValue = await page
+          .locator('#cookie-row0 >> [placeholder="Value"]')
+          .inputValue();
 
         expect(cookieKey).to.equal('Checking cookie Key input!');
         expect(cookieValue).to.equal('Checking cookie Value input!');
@@ -231,7 +236,7 @@ module.exports = () => {
 
     describe('Request body inputs', () => {
       it('body input appears for non GET requests', async () => {
-              // // click and select GET
+        // // click and select GET
         await page.locator('button#rest-method').click();
         await page.locator('text=GET').click();
         let bodyInputcount = await page.locator('#body-entry-select').count();
@@ -244,15 +249,15 @@ module.exports = () => {
         bodyInputcount = await page.locator('#body-entry-select').count();
         expect(bodyInputcount).to.equal(1);
       });
-  
+
       it('can type plain text into body', async () => {
         const input = 'Team Swell is the best!';
         const bodyInput = await page.locator('div.cm-line');
         await bodyInput.fill(input);
-        expect(await page.locator('div.cm-content').innerText()).to.equal(input)
+        expect(await page.locator('div.cm-content').innerText()).to.equal(
+          input
+        );
       });
     });
   }).timeout(20000);
-
-
 };
