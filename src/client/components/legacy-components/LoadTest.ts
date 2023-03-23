059df47032e46aed66cfa3554976814f1a430f37
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-03-22 23:25:29 -0700

    added some comments to understand code better

--- src/client/components/legacy-components/LoadTest.ts
+++ src/client/components/legacy-components/LoadTest.ts
@@ -27,47 +27,63 @@ export async function simpleLoadTest(
   requestsPerSecond: number,
   durationInSeconds: number
 ): Promise<LoadTestResult> {
+  // Initialize variables for start and end times of the load test.
   const startTime = Date.now();
   const endTime = startTime + durationInSeconds * 1000;
+  // Calculate the delay between each request based on the desired requests per second.
   const delayBetweenRequests = 1000 / requestsPerSecond;
 
+  // Initialize counters for sent, received, and missed requests, and for the total response time.
   let totalSent = 0;
   let totalReceived = 0;
   let totalMissed = 0;
   let totalResponseTime = 0;
 
+  // Define the sendRequest function, which sends a request to the target URL and updates the counters.
   const sendRequest = async () => {
     try {
       totalSent += 1;
       const startTime = Date.now();
       const response = await fetch(url);
       const endTime = Date.now();
+      // If the response is successful (HTTP status 200-299), increment the totalReceived counter
+      // and update the totalResponseTime.
       if (response.ok) {
         totalReceived += 1;
         totalResponseTime += endTime - startTime;
       } else {
+        // If the response has a non-success status, increment the totalMissed counter.
         totalMissed += 1;
       }
     } catch (error) {
+      // If there is an error in sending the request, increment the totalMissed counter.
       totalMissed += 1;
     }
   };
 
+  // Define the runUserLoad function, which simulates a single user sending requests to the target URL.
   const runUserLoad = async () => {
+    // Keep sending requests until the end time of the load test is reached.
     while (Date.now() < endTime) {
       await sendRequest();
+      // Wait for the calculated delay between requests before sending the next one.
       await new Promise((resolve) => setTimeout(resolve, delayBetweenRequests));
     }
   };
 
+  // Create an array of userPromises, each representing a concurrent user's load test execution.
   const userPromises = Array(concurrentUsers)
     .fill(null)
     .map(() => runUserLoad());
 
+  // Wait for all userPromises to complete, effectively running the load test.
   await Promise.all(userPromises);
 
-  const averageResponseTime = totalReceived === 0 ? 0 : totalResponseTime / totalReceived;
+  // Calculate the average response time based on the total response time and number of received responses.
+  const averageResponseTime =
+    totalReceived === 0 ? 0 : totalResponseTime / totalReceived;
 
+  // Return the load test results as an object.
   return {
     totalSent,
     totalReceived,
@@ -75,3 +91,4 @@ export async function simpleLoadTest(
     averageResponseTime,
   };
 }
+
