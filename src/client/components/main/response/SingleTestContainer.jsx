250f5f995ef51ee8aba6239438594b1ecd9ae2d0
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-04-06 12:02:40 -0700

    Revive test container functionality
    Remove animated part of the test as it was blocking
    the rest of the functionality to work properly.
    
    Added Semantic HTML to the component.
    
    Some small updates to UX/UI

--- src/client/components/main/response/SingleTestContainer.jsx
+++ src/client/components/main/response/SingleTestContainer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import TestResult from './TestResult';
-import VerticalProgress from './testResultsAnimated';
+import { Box, List, ListItem, Stack } from '@mui/material';
 
 export default function SingleTestContainer({ currentResponse }) {
-  const { request, response } = currentResponse;
+  const { response } = currentResponse;
 
   const { url } = currentResponse;
 
@@ -14,29 +13,37 @@ export default function SingleTestContainer({ currentResponse }) {
 
   if (response.testResult !== undefined && response.testResult !== null) {
     response.testResult.forEach((ele, idx) => {
-      if (ele.status === 'PASS') pass += 1;
+      const { status, message } = ele;
+      if (status === 'PASS') pass += 1;
       else fail += 1;
-      const test = (
-        <TestResult
+      const testResultElement = (
+        <ListItem
           key={`TestResult-${idx}`}
-          id={`TestResult-${idx}`}
-          status={ele.status}
-          message={ele.message}
-        />
+          sx={{ padding: '8px', display: 'flex', justifyContent: 'start' }}
+        >
+          <div
+            className={
+              status === 'PASS'
+                ? 'pass-fail-button-success'
+                : 'pass-fail-button-failure'
+            }
+            id={`TestResult-${idx}-status`}
+          >
+            <strong>{status}</strong>
+          </div>
+          <div id={`TestResult-${idx}-message`} className="pass-fail-response">
+            {message}
+          </div>
+        </ListItem>
       );
 
-      passFailScripts.push(test);
+      passFailScripts.push(testResultElement);
     });
   }
 
-  const data = {
-    datasets: [{ data: [pass, fail], backgroundColor: ['#06a568', '#f66b61'] }],
-    labels: ['Passed', 'Failed'],
-  };
-
   return (
     <>
-      <div className="is-flex cards-titlebar mt-2 mx-2">
+      <Box className="is-flex cards-titlebar mt-2 mx-2">
         <div className="is-flex-grow-1 is-rest is-flex-basis-0 is-flex is-justify-content-center is-align-items-center has-text-weight-medium">
           Tests
         </div>
@@ -45,53 +52,44 @@ export default function SingleTestContainer({ currentResponse }) {
             {url}
           </div>
         </div>
-      </div>
-      <div className="is-neutral-200-box p-3 mx-2">
-        {/* <div>{passFailScripts}</div> */}
-        <div className="mt-2">
+      </Box>
+      <Box className="is-neutral-200-box p-3 mx-2">
+        <Stack className="mt-2">
           <center className="test-results mb-2">Summary</center>
-        </div>
-        <div>
-          <div className="border-top mb-2">
-            {/* Test Results Graph */}
-            <center>
-              <VerticalProgress total={pass + fail} pass={pass} open={open} />
-            </center>
-          </div>
-          {/* Test Results Pass + Fail */}
-          <center>
-            <div className="is-flex is-flex-direction-column">
-              <div className="pass-fail-total mt-5">
-                Total Tests:{' '}
-                <span className="pass-fail-total-number">{pass + fail}</span>
-              </div>
-              <div className="pass-fail-percentage mb-3">
-                Percentage Passed:{' '}
-                <span className="pass-fail-total-number">
-                  {Math.floor((pass / (pass + fail)) * 100)}%
+        </Stack>
+        {/* Test Results Pass + Fail */}
+        <Stack>
+          <section className="is-flex is-flex-direction-column">
+            <article className="pass-fail-total">
+              <span>Total Tests: </span>
+              <span className="pass-fail-total-number">{pass + fail}</span>
+            </article>
+            <article className="pass-fail-percentage mb-3">
+              <span>Percentage Passed: </span>
+              <span className="pass-fail-total-number">
+                {Math.floor((pass / (pass + fail)) * 100)}%
+              </span>
+            </article>
+            <article className="is-flex align-self-end">
+              <div className="pass-fail-button-success">
+                <span>Passed:</span>
+                <span className="ml-3">
+                  <strong>{pass}</strong>
                 </span>
               </div>
-              <div className="is-flex align-self-end">
-                <div>
-                  <span className="has-background-success pass-fail-button">
-                    Passed:
-                  </span>{' '}
-                  <span className="ml-2">{pass}</span>
-                </div>
-                <div className="ml-5">
-                  <span className="has-background-danger pass-fail-button">
-                    Failed:{' '}
-                  </span>{' '}
-                  <span className="ml-2">{fail}</span>
-                </div>
+              <div className="ml-3 mr-3 pass-fail-button-failure">
+                <span>Failed:</span>
+                <span className="ml-3">
+                  <strong>{fail}</strong>
+                </span>
               </div>
-              <div className="is-neutral-200-box p-3 mx-2">
-                <div>{passFailScripts}</div>
-              </div>
-            </div>
-          </center>
-        </div>
-      </div>
+            </article>
+            <List className="is-neutral-200-box p-3 mx-2">
+              {passFailScripts}
+            </List>
+          </section>
+        </Stack>
+      </Box>
     </>
   );
 }
