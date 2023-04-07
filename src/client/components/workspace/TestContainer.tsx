a45844bacd8eff097200f7d32790707f835e2339
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-04-07 10:12:18 -0700

    added tooltip

--- src/client/components/workspace/TestContainer.tsx
+++ src/client/components/workspace/TestContainer.tsx
@@ -51,19 +51,35 @@ const TestContainer: React.FC<TestContainerProps> = ({
   const [runScheduledTests, setScheduledTests] = useState<boolean>(false);
   const [callsPerSecond, setCallsPerSecond] = useState<number>(1);
   const [totalTime, setTotalTime] = useState<number>(10);
-  const [abortController, setAbortController] = useState<AbortController | null>(null);
+  const [abortController, setAbortController] =
+    useState<AbortController | null>(null);
   const isDark = useSelector((state: any) => state.ui.isDark);
 
   const handleShowLoadTest = () => {
     setShowLoadTest(!showLoadTest);
   };
 
-
   const [showLoadTest, setShowLoadTest] = useState(false);
 
-  const reqResObj = currentResponse.url 
-  ? currentResponse : reqResArray.length > 0 
-  ? reqResArray[reqResArray.length - 1] : null;
+  const reqResObj = currentResponse.url
+    ? currentResponse
+    : reqResArray.length > 0
+    ? reqResArray[reqResArray.length - 1]
+    : null;
+
+  const getDisabledReason = () => {
+    if (isTestRunning) {
+      return 'Load test is currently running.';
+    } else if (!reqResObj) {
+      return 'Please add workspace or send request';
+    } else if (!reqResObj.url) {
+      return 'URL is missing.';
+    } else if (reqResObj.request.method !== 'GET') {
+      return 'Only GET requests are supported for load tests.';
+    } else {
+      return '';
+    }
+  };
 
   return (
     <div className="mt-4 mb-4">
@@ -122,33 +138,43 @@ const TestContainer: React.FC<TestContainerProps> = ({
             </div>
             <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center mt-2">
               <div className="ml-2">
-                <button
-                  className={`button is-small is-primary ${
-                    isDark ? '' : 'is-outlined'
-                  } button-padding-vertical button-hover-color ml-3`}
-                  onClick={async () => {
-                    const controller = new AbortController();
-                    setAbortController(controller);
-                    setIsTestRunning(true);
-                    const results = await simpleLoadTest(
-                      reqResObj.url,
-                      callsPerSecond,
-                      totalTime,
-                      controller.signal
-                    );
-                    console.log('reqResObj.request.method',reqResObj.request.method)
-                    // Assuming you have a valid reqResObj
-                    LoadTestController.processLoadTestResults(
-                      reqResObj.id,
-                      results
-                    );
-                    setIsTestRunning(false);
-                  }}
-                  disabled={isTestRunning || !reqResObj || !reqResObj.url || reqResObj.request.method !== 'GET'}
-                >
-                  Run
-                </button>
-                {/* {currently the stop button does not work, no functionality in the loadtest to stop cancel out of the test} */}
+                <div className="tooltip-wrapper">
+                  <button
+                    className={`button is-small is-primary ${
+                      isDark ? '' : 'is-outlined'
+                    } button-padding-vertical button-hover-color ml-3`}
+                    onClick={async () => {
+                      const controller = new AbortController();
+                      setAbortController(controller);
+                      setIsTestRunning(true);
+                      const results = await simpleLoadTest(
+                        reqResObj.url,
+                        callsPerSecond,
+                        totalTime,
+                        controller.signal
+                      );
+                      console.log(
+                        'reqResObj.request.method',
+                        reqResObj.request.method
+                      );
+                      // Assuming you have a valid reqResObj
+                      LoadTestController.processLoadTestResults(
+                        reqResObj.id,
+                        results
+                      );
+                      setIsTestRunning(false);
+                    }}
+                    disabled={
+                      isTestRunning ||
+                      !reqResObj ||
+                      !reqResObj.url ||
+                      reqResObj.request.method !== 'GET'
+                    }
+                  >
+                    Run
+                  </button>
+                  <span className="tooltip-text">{getDisabledReason()}</span>
+                </div>
                 <button
                   className={`button is-small is-danger ${
                     isDark ? '' : 'is-outlined'
@@ -160,15 +186,16 @@ const TestContainer: React.FC<TestContainerProps> = ({
                     }
                     setScheduledTests(false);
                   }}
-                  
                 >
                   Stop
                 </button>
               </div>
             </div>
             <div>
-            Attention: This load test is specifically designed for HTTP GET requests and is intended for backend testing purposes only. 
-            Please be aware that running this test on websites may lead to CORS issues.
+              Attention: This load test is specifically designed for HTTP GET
+              requests and is intended for backend testing purposes only. Please
+              be aware that running this test on websites may lead to CORS
+              issues.
             </div>
           </div>
         </div>
