6f80086d4a30a14f7f97cc4f0c4ebb040b14d1fd
Christopher Suzukida <chris.suzukida@gmail.com>
2023-04-11 01:08:35 -0700

    Remove tooltip, add modal popup on endpoint submit

--- src/client/components/main/MockServerComposer.tsx
+++ src/client/components/main/MockServerComposer.tsx
@@ -1,5 +1,5 @@
 // react-redux
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startServer, stopServer } from '../../../client/toolkit-refactor/mockServer/mockServerSlice';
 import { newRequestFieldsByProtocol } from '../../toolkit-refactor/newRequestFields/newRequestFieldsSlice';
@@ -11,7 +11,7 @@ import CookieEntryForm from './new-request/CookieEntryForm';
 import BodyEntryForm from './new-request/BodyEntryForm';
 
 // mui
-import { Box, Button, styled, Tooltip, TooltipProps, tooltipClasses, Typography } from '@mui/material';
+import { Box, Button, Modal, styled, Tooltip, TooltipProps, tooltipClasses, Typography } from '@mui/material';
 
 /**
  * grab context from Electron window
@@ -23,7 +23,11 @@ const { api } = (window as any);
 // TODO: add an option to see the list of existing routes that shows up in the response window
 // TODO: add endpoint validation
 // TODO: add the ability to mock HTML responses (or remove the HTML option from the BodyEntryForm component)
+// TODO: hook up the headers and cookies to the mock endpoint creation
 
+/**
+ * Tooltip styling
+ */
 const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
 ))(({ theme }) => ({
@@ -36,7 +40,23 @@ const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
   },
 }));
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const MockServerComposer = (props) => {
+  const [isMockServer, setIsMockServer] = useState(true);
+  const [modalText, setModalText] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [userDefinedEndpoint, setUserDefinedEndpoint] = useState('');
   const dispatch = useDispatch();
   
   // grab the isServerStarted state from the Redux store
@@ -96,16 +116,33 @@ const MockServerComposer = (props) => {
       // send a message with the stringified postData to the main_mockServerController to execute the POST request 
       api.send('submit-mock-request', JSON.stringify(postData));
 
-      // TODO: figure out how to populate the response window with a success message and instructions on how to view the mock response. For now it will just alert the user.
-      alert(`Endpoint submitted! 
+      // TODO: figure out how to populate the response window with a success message and instructions on how to view the mock response. For now it will just show a modal with the instructions
+      const responseString = `Endpoint submitted! 
       Please make a request to: 
-      http://localhost:9990${parsedUserDefinedEndpoint} to see the response.`);
+      http://localhost:9990${parsedUserDefinedEndpoint} to see the response.`;
+
+      setUserDefinedEndpoint(parsedUserDefinedEndpoint);
+      setModalText(responseString);
+      setShowModal(true);
     } else {
       alert('Please start the mock server before submitting an endpoint');
     }
   }
 
-  console.log(props);
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setModalText('');
+  }
+
+  // instructions to pass down to the BodyEntryForm component as a placeholder
+  const instructions = `
+  How to create a mock endpoint:
+
+  1. Click the start server button
+  2. Select a method type from the dropdown menu
+  3. Enter an endpoint and a response to mock
+  4. Click the submit button 
+  `;
 
   return (
     <Box
@@ -119,53 +156,25 @@ const MockServerComposer = (props) => {
       id="mockcomposer-http2"
     >
       <div className="container-margin">
-        <HtmlTooltip placement="bottom-end" title={
-          <>
-            <Typography variant="body1">
-              <b>To create a mock response:</b>
-              <br />
-            </Typography>
-            <Typography variant="body2">
-              1. Click "Start Server".
-              <br />
-              2. Select a request type from the dropdown.
-              <br />
-              3. Enter your desired /endpoint.
-              <br />
-              4. Define your desired response in the body. 
-              <br />
-              5. Click "Submit".
-            </Typography>
-            <br />
-            <Typography variant="body1">
-              <b>To view your response:</b>
-            </Typography>
-            <Typography variant="body2">
-              Make a request to: localhost:9990/yourendpoint
-            </Typography>  
-          </>
-        }
-        >
-          <div className="is-flex is-align-items-center">
-            <Button 
-              className="button is-normal is-primary-100 add-request-button is-vertical-align-center is-justify-content-center no-border-please" 
-              id="response" 
-              variant="contained" 
-              color="primary" 
-              onClick={handleServerButtonClick}
-              sx={{ mr: 1, textTransform: 'none' }}>
-                {isServerStarted ? 'Stop Server' : 'Start Server'}
-            </Button>
-            <RestMethodAndEndpointEntryForm 
-              {...props}
-              method={props.newRequestFields.method}
-              placeholder='/Enter Mock Endpoint'
-            />
-          </div>
-        </HtmlTooltip>
+        <div className="is-flex is-align-items-center">
+          <Button 
+            className="button is-normal is-primary-100 add-request-button is-vertical-align-center is-justify-content-center no-border-please" 
+            id="response" 
+            variant="contained" 
+            color="primary" 
+            onClick={handleServerButtonClick}
+            sx={{ mr: 1, textTransform: 'none' }}>
+              {isServerStarted ? 'Stop Server' : 'Start Server'}
+          </Button>
+          <RestMethodAndEndpointEntryForm 
+            {...props}
+            method={props.newRequestFields.method}
+            placeholder='/Enter Mock Endpoint'
+          />
+        </div>
         <HeaderEntryForm {...props} />
         <CookieEntryForm {...props} />
-        <BodyEntryForm {...props} /> 
+        <BodyEntryForm isMockServer placeholder={instructions} {...props} /> 
         <div className="is-flex">
           <Button 
             className="button is-normal is-primary-100 add-request-button is-vertical-align-center is-justify-content-center no-border-please" 
@@ -175,6 +184,28 @@ const MockServerComposer = (props) => {
             sx={{ ml : 1, textTransform: 'none' }}>
               Submit
           </Button>
+          <Modal
+            open={showModal}
+            onClose={handleCloseModal}
+          >
+            <Box sx={style} className="is-flex is-flex-direction-column">
+              <div>
+                <Typography variant="h6">
+                  Mock endpoint successfully created!
+                  <br />
+                  <br />
+                  To view the response visit: 
+                  <br />
+                  localhost:9990{userDefinedEndpoint}
+                </Typography>
+              </div>
+              <div className="is-flex is-justify-content-flex-end">
+                <Button onClick={handleCloseModal}>
+                  Close
+                </Button>
+              </div>
+            </Box>
+          </Modal>
         </div>
       </div>
     </Box>
