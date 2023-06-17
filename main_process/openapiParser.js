4507aa1756af0e08ab125aba08d16e54838b0c00
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-06-17 13:23:41 -0700

    Revert "update openApiParser"
    This reverts commit 9cfdb05690c846e43da38ce79c909d69070ecb2c.

--- main_process/openapiParser.js
+++ main_process/openapiParser.js
@@ -1,28 +1,8 @@
 const YAML = require('yamljs');
 
-/////////////
-const Ajv = require('ajv');
-const openapiSchema = require('openapi-schema-validation');
-
-const ajv = new Ajv();
-const validateOpenAPI = ajv.compile(openapiSchema);
-
-
-const errors = validateOpenAPI.errors;
-if (errors) {
-  const errorMessages = errors.map((error) => `Validation Error: ${error.dataPath} ${error.message}`);
-  throw new Error(`Invalid OpenAPI document.\n${errorMessages.join('\n')}`);
-}
-/////////////
-
 // TODO The security keys need to be implmented into the OpenApi request
 
 const openapiParserFunc = (input) => {
-
-  // Input validation
-  if (typeof input !== 'string') {
-    throw new TypeError('Input must be a string.');
-  }
 
   if (input === undefined || input === null) {
     throw new ReferenceError('OpenAPI Document not found.');
@@ -30,72 +10,38 @@ const openapiParserFunc = (input) => {
   // Parse the input into JSON or YAML
   let doc;
   try {
-    //try json parse
     doc = JSON.parse(input);
-  } catch (jsonError) {
-    // try to parse as yaml
-    try{
-      doc = YAML.parse(input)
-    } catch (yamlError) {
-      throw new Error('Invalid JSON, or YAML format: ' + yamlError.message)
-    }
-  }
-
-  // Schema validation
-  const isValidOpenAPI = validateOpenAPI(doc);
-  if (!isValidOpenAPI) {
-    throw new Error('Invalid OpenAPI document. Schema validation failed.');
+  } catch (SyntaxError) {
+    doc = YAML.parse(input);
   }
  
- 
-  const { info = {}, servers = [], tags = [], paths = {}, components = {} } = doc;
+  const { info, servers, tags, paths, components } = doc;
   
   info.openapi = doc.openapi;
 
-  const serverUrls = servers.map((server) => server.url);
-
-  const openapiReqArray = [];
+  let serverUrls
+  if (servers) {
+    serverUrls = [...servers.map((server) => server.url)];
+  } else {
+    serverUrls = []
+  }
   let id = 0;
 
+  const openapiReqArray = [];
   Object.entries(paths).forEach(([endpoint, pathObj]) => {
     Object.entries(pathObj).forEach(([method, operationObj]) => {
       id += 1;
-
       const {
-        summary = '',
-        description = '',
-        operationId = '',
-        tags = [],
-        parameters = [],
-        security = [],
-        responses = {},
-        externalDocs = {},
-        version = '',
+        summary,
+        description,
+        operationId,
+        tags,
+        parameters, // security
       } = operationObj;
 
-      const securitySchemes = components.securitySchemes || {};
-      const responseExamples = {}; // Extract response examples from responses object if available
-
-      // const request = {
-      //   id,
-      //   // enabled: true,
-      //   reqTags: tags,
-      //   summary,
-      //   description,
-      //   operationId,
-      //   method: method.toUpperCase(),
-      //   reqServers: [],
-      //   endpoint,
-      //   parameters,
-      //   body: new Map(),
-      //   headers: {},
-      //   cookies: {},
-      //   // params: {},
-      //   // queries: {},
-      //   urls: [],
-      // };
       const request = {
         id,
+        // enabled: true,
         reqTags: tags,
         summary,
         description,
@@ -107,10 +53,8 @@ const openapiParserFunc = (input) => {
         body: new Map(),
         headers: {},
         cookies: {},
-        securitySchemes,
-        responseExamples,
-        externalDocs,
-        version,
+        // params: {},
+        // queries: {},
         urls: [],
       };
       openapiReqArray.push(request);
