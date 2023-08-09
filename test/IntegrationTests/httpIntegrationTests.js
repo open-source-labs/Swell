66acae8cdf68417934db27cc58ba77a7fe1745da
Nitesh Manem <nacho@Nachos-MBP.fios-router.home>
2023-08-09 00:56:00 -0400

    updated test file

--- test/IntegrationTests/httpIntegrationTests.js
+++ test/IntegrationTests/httpIntegrationTests.js
@@ -6,8 +6,11 @@ chai.use(chaiHttp);
 const path = require('path');
 const fs = require('fs');
 
+// const { RootState } = require('../../src/client/toolkit-refactor/store.ts');
+
 let electronApp,
   page,
+  RootState,
   num = 0;
 
 //TODO: May need to wrap this test tile in the following export below
@@ -22,16 +25,27 @@ describe('HTTP Integration Tests', function () {
   //~ Launch app
   before(async () => {
     electronApp = await electron.launch({ args: [projectPath] });
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    page = await electronApp.windows()[0]; // In case there is more than one window
+    await page.waitForLoadState(`domcontentloaded`);
   });
 
-  //~ Clear database and workspace before each request
-  beforeEach( async () => {
-    
-    
-    await page.locator('button >> text=Clear Workspace').click();
+  //~ Reset before each request
+  beforeEach(async () => {
+    // clear workspace
+    if (page) await page.locator('button >> text=Clear Workspace').click();
+
+    // clear DB
+    await new Promise((resolve, reject) => {
+      chai
+        .request('http://localhost:3004')
+        .get('/clear')
+        .end((err, res) => {
+          if (err) reject(err);
+          else resolve(res);
+        });
+    });
   })
-  
-  
 
   // //~ If failed, add screenshot to folder
   // afterEach(async function () {
@@ -48,14 +62,32 @@ describe('HTTP Integration Tests', function () {
   //   }
   // });
 
-  //~ close electron app when complete
-  after(async () => {
-    await page.locator('button >> text=Clear Workspace').click();
-    await electronApp.close();
-  });
+  // //~ close electron app when complete
+  // after(async () => {
+  //   console.log('here')
+  //   if (page) await page.locator('button >> text=Clear Workspace').click();
+  //   await electronApp.close();
+  // });
 
-  describe('HTTP POST Request Test', () => {
-    it('sample test...', () => {
+  describe('HTTP POST Request Test', async () => {
+    const url = 'http://localhost:3004/book';
+
+    // // Load RootState before running the tests
+    // before(async () => {
+    //   const module = await import('../../src/client/toolkit-refactor/store.ts');
+    //   RootState = module.RootState;
+    // });
+
+    //& check if state changed
+    it('Changes newRequestFields state to have correct url', async () => {
+      await page.locator('#url-input').fill(url);
+      // await console.log(RootState.newRequestFields)
+      // Fetch and log the Redux state from Electron app
+      // const reduxState = await page.evaluate(() => window.getReduxState());
+      const reduxState = window.api.getReduxState();
+      
+      console.log(reduxState);
+
     });
   });
 
