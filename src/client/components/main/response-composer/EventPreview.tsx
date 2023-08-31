89a73b2d3430eb89a9bf7233633b8481604f8ac4
Parkreiner <throwawayclover@gmail.com>
2023-08-31 00:11:42 -0400

    minor: Add useAppSelector

--- src/client/components/main/response-composer/EventPreview.tsx
+++ src/client/components/main/response-composer/EventPreview.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useAppSelector } from '~/toolkit/store';
 import dropDownArrow from '../../../../assets/icons/caret-down-tests.svg';
 import dropDownArrowUp from '../../../../assets/icons/caret-up-tests.svg';
 
@@ -13,12 +13,14 @@ interface Props {
 const EventPreview: React.FC<Props> = ({ contents }) => {
   const [showPreview, setShowPreview] = useState(false);
   const handleShowPreview = () => setShowPreview(!showPreview);
-  const isDark = useSelector((state: any) => state.ui.isDark);
+  const isDark = useAppSelector((state) => state.ui.isDark);
 
   return (
     <div>
       <div
-        className={`${isDark ? 'is-dark-500' : ''} is-rest-invert show-hide-event cards-dropdown minimize-card is-flex is-align-items-center is-justify-content-center`}
+        className={`${
+          isDark ? 'is-dark-500' : ''
+        } is-rest-invert show-hide-event cards-dropdown minimize-card is-flex is-align-items-center is-justify-content-center`}
         onClick={() => {
           handleShowPreview();
         }}
@@ -51,7 +53,10 @@ const EventPreview: React.FC<Props> = ({ contents }) => {
         )}
       </div>
       {showPreview === true && (
-        <div style={{ height: '100vh' }} className={`${isDark ? 'is-dark-500' : ''} is-full`}>
+        <div
+          style={{ height: '100vh' }}
+          className={`${isDark ? 'is-dark-500' : ''} is-full`}
+        >
           <iframe
             srcDoc={JSON.parse(contents)}
             title="output"
