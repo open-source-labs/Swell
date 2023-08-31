3eec0b353764a23fa57403c0fe284e72e52bf319
Parkreiner <throwawayclover@gmail.com>
2023-08-30 22:23:14 -0400

    feat: Add useAppSelector hook and update documentation

--- src/client/toolkit-refactor/store.ts
+++ src/client/toolkit-refactor/store.ts
@@ -1,37 +1,54 @@
 /**
  * Defines the top-level Redux store that should be used throughout the entire
- * app for non-model data.
+ * app for global state, as well as any related utiliites.
  * {@link https://redux-toolkit.js.org/}
  *
- * Please do not put temporary form state in the store. All of that can be done
- * with local state management, via things like React hooks and the Context API.
+ * Please do not put any arbitrary state values in here. useState and useReducer
+ * can be used for state that only belongs to one component. Form state should
+ * almost never go in here. Redux state is still global state, and even when you
+ * opt into Redux's Flux Standard Actions approach, it can be hard to debug.
  *
- * @todo Replace all instances of Date objects in the Redux store with timestamp
- * strings. Redux will complain that the data isn't serializable, because Dates
- * aren't part of the JSON spec, but when you call JSON.stringify on an object
- * that contains Dates, they'll automatically have their .toString method
- * called, turning them into timestamp strings.
+ * Also, please do not use the raw useSelector or useDispatch hooks from the
+ * react-redux library. These are not type-safe. Use the useAppSelector and
+ * useAppDispatch hooks exported here. They work the same, but have type safety.
  *
- * So basically, the data technically is serializable, but Redux can't tell that
- * and will keep yelling at you in the console until all the Dates are replaced.
+ * Reminder: all values in a Redux store must be JSON-serializable values in
+ * order to be proper. Only these types are valid (defined recursively):
+ * - Strings
+ * - Numbers
+ * - Booleans
+ * - null
+ * - Arrays of JSON values
+ * - Objects of JSON values (instantiated objects like Date are not allowed)
+ *
+ * All other values (including undefined and functions) are not valid.
+ *
+ * @todo With that said, parts of the Redux store still have Date objects, and
+ * by default, Redux will yell at you. In practice, it shouldn't cause problems
+ * (trying to access a Date object as a string will automatically call its
+ * .toString method on it), but because Redux doesn't know that, it'll keep
+ * yelling. All Dates should be replaced with date strings or timestamp numbers
  */
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
 import rootReducer from './rootReducer';
+import {
+  type TypedUseSelectorHook,
+  useSelector,
+  useDispatch,
+} from 'react-redux';
 
 /**
- * The top-level redux store. serializableCheck was previously turned off, which
- * stops Redux from complaining at you, but the Redux store should only have
- * serializable JSON values where possible.
- *
- * @todo Remove any non-serializable values from the store (namely, replace
- * Date objects with Date strings).
+ * The top-level redux store.
  */
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
+      /**
+       * @todo Remove any non-serializable values from the store (namely,
+       * replace Date objects with strings/numbers), and flip this to true.
+       */
       serializableCheck: false,
     }),
 });
@@ -40,14 +57,20 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 /**
+ * The Redux store's dispatch function, exposed directly; needed for files that
+ * are not React function components.
+ */
+export const appDispatch = store.dispatch;
+
+/**
  * A type-safe version of React-Redux's useDispatch hook.
  */
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
 /**
- * The Redux store's dispatch function, exposed directly; needed for files that
- * are not React functional components.
+ * A type-safe version of React-Redux's useSelector hook.
  */
-export const appDispatch = store.dispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 export default store;
 
