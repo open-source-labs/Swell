786a7dcbfb33d21e89655800d2ceb9a2ceadaab2
Parkreiner <throwawayclover@gmail.com>
2023-08-31 00:01:24 -0400

    refactor: Clean up GraphQLIntrospectionLog.tsx

--- src/client/components/main/GraphQL-composer/GraphQLIntrospectionLog.tsx
+++ src/client/components/main/GraphQL-composer/GraphQLIntrospectionLog.tsx
@@ -1,44 +1,38 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { GraphQLSchema } from 'graphql';
-
-import graphQLController from '../../../controllers/graphQLController';
-import TextCodeArea from '../sharedComponents/TextCodeArea';
-import { RootState } from '../../../toolkit-refactor/store';
-
-interface IntrospectionData {
-  schemaSDL: string | null;
-  clientSchema: GraphQLSchema | null;
-}
+import { useAppSelector } from '~/toolkit/store';
+import graphQLController from '~/controllers/graphQLController';
+import TextCodeArea from '~/components/main/sharedComponents/TextCodeArea';
 
 const GraphQLIntrospectionLog: React.FC = () => {
-  const headers = useSelector(
-    (store: RootState) => store.newRequest.newRequestHeaders.headersArr
-  );
-  const cookies = useSelector(
-    (store: RootState) => store.newRequest.newRequestCookies.cookiesArr
-  );
-  const introspectionData: (IntrospectionData | string) = useSelector(
-    (store: RootState) => store.introspectionData
-  );
-  const url: string = useSelector(
-    (store: RootState) => store.newRequestFields.url
-  );
-  const isDark: boolean = useSelector((store: RootState) => store.ui.isDark);
+  const newReq = useAppSelector((store) => store.newRequest);
+  const url = useAppSelector((store) => store.newRequestFields.url);
+  const introspectionData = useAppSelector((store) => store.introspectionData);
+  const isDark = useAppSelector((store) => store.ui.isDark);
+
+  const headers = newReq.newRequestHeaders.headersArr;
+  const cookies = newReq.newRequestCookies.cookiesArr;
 
   return (
     <div>
       <button
-        className={`${isDark ? 'is-dark-200' : ''} button is-small add-header-or-cookie-button`}
+        className={`${
+          isDark ? 'is-dark-200' : ''
+        } button is-small add-header-or-cookie-button`}
         onClick={() => graphQLController.introspect(url, headers, cookies)}
       >
         Introspect
       </button>
       <div id="gql-introspection">
-        {introspectionData === 'Error: Please enter a valid GraphQL API URI' && (
+        {/**
+         * @todo 2023-08-30 - Only formatted code; logic untouched. Need to
+         * figure out what this condition was trying to account for.
+         */}
+        {introspectionData ===
+          'Error: Please enter a valid GraphQL API URI' && (
           <div>{introspectionData}</div>
         )}
-        {!!introspectionData.schemaSDL && (
+
+        {introspectionData.schemaSDL !== null && (
           <TextCodeArea
             value={introspectionData.schemaSDL}
             mode="application/json"
@@ -52,57 +46,3 @@ const GraphQLIntrospectionLog: React.FC = () => {
 };
 
 export default GraphQLIntrospectionLog;
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import graphQLController from '../../../controllers/graphQLController';
-// import TextCodeArea from '../new-request/TextCodeArea.tsx';
-
-// const GraphQLIntrospectionLog = () => {
-//   const headers = useSelector(
-//     (store) => store.newRequest.newRequestHeaders.headersArr
-//   );
-//   const cookies = useSelector(
-//     (store) => store.newRequest.newRequestCookies.cookiesArr
-//   );
-//   const introspectionData = useSelector((store) => store.introspectionData);
-//   const url = useSelector((store) => store.newRequestFields.url);
-//   const isDark = useSelector((store) => store.ui.isDark);
-
-//   return (
-//     <div>
-//       <button
-//         className={`${
-//           isDark ? 'is-dark-200' : ''
-//         } button is-small add-header-or-cookie-button`}
-//         onClick={() => graphQLController.introspect(url, headers, cookies)}
-//       >
-//         Introspect
-//       </button>
-//       <div id="gql-introspection">
-//         {introspectionData ===
-//           'Error: Please enter a valid GraphQL API URI' && (
-//           <div>{introspectionData}</div>
-//         )}
-//         {!!introspectionData.schemaSDL && (
-//           <TextCodeArea
-//             value={introspectionData.schemaSDL}
-//             mode="application/json"
-//             onChange={() => {}}
-//             readOnly={true}
-//           />
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-// export default GraphQLIntrospectionLog;
