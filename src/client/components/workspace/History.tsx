b9cdd69291fbcf339a2339ee3aff6116230492d1
Parkreiner <throwawayclover@gmail.com>
2023-08-31 23:49:57 -0400

    fix: Remove invalid file extension from import path

--- src/client/components/workspace/History.tsx
+++ src/client/components/workspace/History.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
-import { useAppDispatch } from '~/toolkit/store.js';
+import { useAppDispatch } from '~/toolkit/store';
 import { historyDeleted } from '../../toolkit-refactor/slices/historySlice';
 import { setSidebarActiveTab } from '../../toolkit-refactor/slices/uiSlice';
 
