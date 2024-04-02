ff523968af6ba4655e15a0e00477765840965bbf
kkrzywon <kkrzywon@gmail.com>
2024-04-02 15:06:44 -0400

    Inverted the IMG color for dark mode for Empty Response, and adjusted some colors schemes

--- src/client/components/main/response-composer/EmptyState.tsx
+++ src/client/components/main/response-composer/EmptyState.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import logofaded from '../../../../assets/img/swell-logo-faded.avif';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../client/toolkit-refactor/store';
 
 interface EmptyStateProps {
   connection?: any;
 }
 
+
 export default function EmptyState({ connection }: EmptyStateProps) {
+  const isDark = useSelector((store: RootState) => store.ui.isDark);
+
   return (
     <div className="empty-state-wrapper">
-      <img className="empty-state-img" src={logofaded} alt="faded-logo" />
+      <img
+        className="empty-state-img"
+        src={logofaded}
+        alt="faded-logo"
+        style={{ filter: isDark ? 'invert(1)' : 'none' }}
+      />
     </div>
   );
 }
