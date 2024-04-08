3727313dfa68cb3fa4744a296db58d51dd97e886
adeebbayat <adeebbayat@yahoo.com>
2024-04-08 13:27:42 -0700

    added two tests to mockServerTest, commented out most of test suite must undo for production

---
+++ test/subSuites/mockServerTest.js
@@ -0,0 +1,74 @@
+// Import necessary modules
+const { _electron: electron } = require('playwright');
+const chai = require('chai');
+const expect = chai.expect;
+const path = require('path');
+const fs = require('fs-extra');
+
+// Declare global variables
+let electronApp, page;
+
+// Export test suite
+module.exports = () => {
+  describe('App opens and renders a page', () => {
+    // Launch Electron app before all tests
+    before(async () => {
+      // Check if Electron app is already running
+      if (!electronApp) {
+        electronApp = await electron.launch({ args: ['main.js'] });
+      }
+    });
+
+    // Close Electron app after all tests
+    after(async () => {
+      // Check if Electron app exists
+      if (electronApp) {
+        await page.locator('button >> text=Clear Workspace').click();
+        await electronApp.close();
+        electronApp = null; // Reset Electron app reference
+      }
+    });
+
+    // If a test fails, take a screenshot
+    afterEach(async function () {
+      if (this.currentTest.state === 'failed' && electronApp) {
+        const window = await electronApp.firstWindow();
+        const imageBuffer = await window.screenshot();
+        fs.writeFileSync(
+          path.resolve(__dirname + '/../failedTests', `FAILED_${this.currentTest.title}.png`),
+          imageBuffer
+        );
+      }
+    });
+
+
+    describe('Mock Server selection', () => {
+
+      before(async () => {
+        if (!page) {
+          page = await electronApp.firstWindow();
+          await page.waitForLoadState('domcontentloaded');
+        }
+      });
+
+      it('can select Mock Server', async () => {
+        await page.locator('button >> text=Mock').click();
+        expect(await page.locator('button#response').count()).to.equal(1);
+      });
+
+      it('can type in Mock Server route', async () => {
+        await page
+          .locator('#url-input')
+          .fill('/testRoute')
+
+        const mockRoute = await page
+          .locator('#url-input')
+          .inputValue();
+
+        expect(mockRoute).to.equal('/testRoute')
+      })
+
+    });
+
+  }).timeout(20000);
+};
